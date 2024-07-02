ae196d86755c1e77b15983ad41022371392e70b4
Valerio Gherardi <vgherard840@gmail.com>
2024-07-02 14:14:24 +0200

    Add "Albufera Outflows" data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Albufera Outflows Daily Data
+#'
+#' Data from continuous measurements of outflow rates and lake level from
+#' "Confederación Hidrográfica del Júcar".
+#'
+#' Datos de los medidores en continuo de las salidas del lago.
+#'
+#'
+#' @format ## `albufera_outflows`
+#' A data frame with 6140 rows and 5 columns:
+#' \describe{
+#'   \item{Date}{Date of measurement}
+#'   \item{Level}{Lake level (in meters above sea level)}
+#'   \item{Pujol}{Outflow at Pujol (meters cube per second)}
+#'   \item{Perellonet}{Outflow at Perellonet (meters cube per second)}
+#'   \item{Perello}{Outflow at Perello (meters cube per second)}
+#' }
+#' @source <https://aps.chj.es/>
+"albufera_outflows"
---
+++ data-raw/albufera_outflows.R
@@ -0,0 +1,45 @@
+## code to prepare `albufera_outflows` dataset goes here
+
+local({
+  CHJ <- readr::read_delim("data-raw/CHJ.csv",
+                           delim = ";",
+                           escape_double = FALSE,
+                           trim_ws = TRUE)
+
+  res <- CHJ |>
+    dplyr::filter(INDI_CHJ %in% c("08A02", "08A03", "08A04")) |>
+    dplyr::select(Fecha, `Caudal (m³/s)`, INDI_CHJ) |>
+    tidyr::pivot_wider(names_from = INDI_CHJ, values_from = `Caudal (m³/s)`)
+
+  level <- CHJ |>
+    dplyr::filter(INDI_CHJ %in% "08A01") |>
+    dplyr::select(Fecha, `Nivel (m.s.n.m)` , INDI_CHJ) |>
+    tidyr::pivot_wider(names_from = INDI_CHJ, values_from = `Nivel (m.s.n.m)` )
+
+  res <- merge(level, res, by= "Fecha", all = T)
+
+  res$Fecha <- lubridate::dmy(sub(" 00:00:00", "", res$Fecha))
+
+  names(res) <- c("Date", "Level", "Pujol", "Perellonet", "Perello")
+
+  res$Level <- as.numeric(sub(",", ".", res$Level))
+
+  res$Pujol <- as.numeric(sub(",", ".", res$Pujol))
+
+  res$Perellonet <- as.numeric(sub(",", ".", res$Perellonet))
+
+  res$Perello <- as.numeric(sub(",", ".", res$Perello))
+
+  res = res |>
+    dplyr::mutate(
+      Pujol = replace(Pujol, lubridate::year(Date) == 2017, NA),
+      Perellonet = replace (Perellonet, lubridate::year(Date) == 2017, NA),
+      Perello = replace(Perello, lubridate::year(Date) == 2017, NA)
+      )
+
+  albufera_outflows <- res
+
+  usethis::use_data(albufera_outflows, overwrite = TRUE)
+})
+
+
