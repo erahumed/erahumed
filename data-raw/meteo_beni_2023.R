e8d379017c49a37c91d1b9d0905094d7c73cd12c
Valerio Gherardi <vgherard840@gmail.com>
2024-07-03 10:33:42 +0200

    Fix meteo generation script

--- data-raw/meteo_beni_2023.R
+++ data-raw/meteo_beni_2023.R
@@ -1,9 +1,13 @@
 ## code to prepare `meteo_beni_2023` dataset goes here
 
 local({
-  meteo_beni_2023 <- readxl::read_excel("data-raw/raw/meteo_beni_2023.xlsx")
-  colnames(meteo_beni_2023)[1] = "Fecha"
-  meteo_beni_2023$Date = lubridate::as_date(meteo_beni_2023$Fecha)
+  meteo_beni_2023 <-
+    readxl::read_excel("data-raw/raw/meteo_beni_2023.xlsx",
+                       range = cellranger::cell_cols("A:I")
+                       ) |>
+    dplyr::rename(Date = FECHA) |>
+    dplyr::mutate(Date = as.Date(Date))
+
   usethis::use_data(meteo_beni_2023, overwrite = TRUE)
 })
 
