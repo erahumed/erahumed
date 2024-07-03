7c55060efce76c8cbabaada9362344107395afc8
Valerio Gherardi <vgherard840@gmail.com>
2024-07-03 10:13:46 +0200

    Format dates with as.Date() rather than {lubridate}

--- data-raw/albufera_outflows.R
+++ data-raw/albufera_outflows.R
@@ -6,40 +6,26 @@ local({
                            escape_double = FALSE,
                            trim_ws = TRUE)
 
-  res <- CHJ |>
-    dplyr::filter(INDI_CHJ %in% c("08A02", "08A03", "08A04")) |>
-    dplyr::select(Fecha, `Caudal (m³/s)`, INDI_CHJ) |>
-    tidyr::pivot_wider(names_from = INDI_CHJ, values_from = `Caudal (m³/s)`)
+  chj_stations <- c("08A01" = "Level",
+                    "08A02" = "Pujol",
+                    "08A03" = "Perellonet",
+                    "08A04" = "Perello")
 
-  level <- CHJ |>
-    dplyr::filter(INDI_CHJ %in% "08A01") |>
-    dplyr::select(Fecha, `Nivel (m.s.n.m)` , INDI_CHJ) |>
-    tidyr::pivot_wider(names_from = INDI_CHJ, values_from = `Nivel (m.s.n.m)` )
-
-  res <- merge(level, res, by= "Fecha", all = T)
-
-  res$Fecha <- lubridate::dmy(sub(" 00:00:00", "", res$Fecha))
-
-  names(res) <- c("Date", "Level", "Pujol", "Perellonet", "Perello")
-
-  res$Level <- as.numeric(sub(",", ".", res$Level))
-
-  res$Pujol <- as.numeric(sub(",", ".", res$Pujol))
-
-  res$Perellonet <- as.numeric(sub(",", ".", res$Perellonet))
-
-  res$Perello <- as.numeric(sub(",", ".", res$Perello))
-
-  res = res |>
+  albufera_outflows <- CHJ |>
+    dplyr::filter(INDI_CHJ %in% names(chj_stations)) |>
+    dplyr::transmute(
+      Date = Fecha,
+      name = chj_stations[INDI_CHJ],
+      value = ifelse(name == "Level", `Nivel (m.s.n.m)`, `Caudal (m³/s)`)
+      ) |>
+    dplyr::mutate(Date = as.Date(Date, format = "%d-%m-%Y")) |>
+    dplyr::mutate(value = as.numeric(sub(",", ".", value))) |>
     dplyr::mutate(
-      Pujol = replace(Pujol, lubridate::year(Date) == 2017, NA),
-      Perellonet = replace (Perellonet, lubridate::year(Date) == 2017, NA),
-      Perello = replace(Perello, lubridate::year(Date) == 2017, NA)
-      )
-
-  albufera_outflows <- res
+      value = replace(value,
+                      name != "Level" & lubridate::year(Date) == 2017,
+                      NA)
+      ) |>
+    tidyr::pivot_wider(names_from = name, values_from = value)
 
   usethis::use_data(albufera_outflows, overwrite = TRUE)
 })
-
-
