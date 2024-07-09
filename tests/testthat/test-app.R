452f4ddb4562124b57da0b65e8bf15d7358fd321
Valerio Gherardi <vgherard840@gmail.com>
2024-07-09 11:10:05 +0200

    Add server tests

---
+++ tests/testthat/test-app.R
@@ -0,0 +1,14 @@
+library(shiny)
+
+testServer(shiny_server, {
+  session$setInputs(
+    date_range = as.Date(c("2013-01-01", "2014-12-31")),
+    variable = "level"
+    )
+
+  d <- hb_data()
+  expect_equal(min(d$date), input$date_range[1])
+  expect_equal(max(d$date), input$date_range[2])
+
+  output$hb_plot
+})
