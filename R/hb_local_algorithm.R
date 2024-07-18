e3c2d88f155c1a59bbc323f7dac6df3f396c895a
Valerio Gherardi <vgherard840@gmail.com>
2024-07-18 10:52:25 +0200

    Add draft compute_ideal_outflows()

--- R/hb_local_algorithm.R
+++ R/hb_local_algorithm.R
@@ -1,5 +1,55 @@
-propagate_ditch <- function(., lag) {
-  ord <- match(.$cluster_id, lag$cluster_id)
+propagate_ditch <- function(., .lag) {
+  ord <- match(.$cluster_id, .lag$cluster_id)
+
+  .$lag_accum_drain <- .lag$accum_drain[ord]
+  .$lag_accum_drain <- pmax(
+    .$lag_accum_drain + (.$petp < 0 & .$lag_accum_drain > 0) * .$petp_m3_s,
+    0)
+
+  .$lag_accum_rain <- .lag$accum_rain[ord]
+  .$lag_accum_rain <- .$lag_accum_rain +
+    (.$petp < 0 & .$lag_accum_rain > 0 & .$lag_accum_drain <= 0) * .$petp_m3_s
+
+  .$lag_accum_rain_cm <- .$lag_accum_rain * 100 * erahumed:::s_per_day() / .$area
+  .$lag_accum_rain <- pmax(.$lag_accum_rain, 0)
+
+  .$Evap_mismatch <-
+    (.$irrigation & .$draining & .lag$accum_rain[ord] > 0 & .$petp < 0) *
+    pmin(.$lag_accum_rain_cm + .$petp_cm, 0)
+
+  .$corrected <- isTRUE(.$lag_accum_drain > 0 & (!.$draining | .$irrigation))
+
+  idxs <- cumsum(.$corrected) > 0
+  .$irrigation[idxs] <- .lag$irrigation[ord][idxs]
+  .$draining[idxs] <- .lag$draining[ord][idxs]
+  .$height_diff_cm[idxs] <- .lag$height_diff_cm[ord][idxs]
+
+  return(.)
+}
+
+compute_accum_pt1 <- function(., ideal_flow_rate = 5) {
+  .$inflow <- .$irrigation * (
+    .$draining * ideal_flow_rate +
+    (1-.$draining) * (.$height_diff_cm - .$petp_cm)
+  )
+  .$outflow <- ifelse(.$lag_accum_drain > 0 & .$petp < 0,
+                      pmax(.$inflow - .$height_diff_cm, 0),
+                      pmax(.$inflow + .$petp_cm - .$height_diff_cm, 0)
+  )
+  .$outflow_m3 <- .$outflow * .$area / 100 / erahumed:::s_per_day()
+  .$outflow_rain <- (.$petp > 0) * .$petp * .$area / 1000 / erahumed:::s_per_day()
+  .$outflow_phys <- .$draining * (.$outflow_m3 - .$outflow_rain)
+  .$outflow_flux <- .$irrigation * .$outflow_phys
+  .$outflow_drain <- (1-.$irrigation) * .$outflow_phys
+  .$outflow_drain <-
+    .$corrected * .$lag_accum_drain + (1 - .$corrected) * .$outflow_drain
+  .$outflow_rain <- .$outflow_rain + .$lag_accum_rain
+
+  .
+}
+
+compute_ideal_outflows <- function(., lag) {
+  ord <- match(.$cluster_id, .lag$cluster_id)
 
   .$lag_accum_drain <- lag$accum_drain[ord]
   .$lag_accum_drain <- pmax(
@@ -17,17 +67,15 @@ propagate_ditch <- function(., lag) {
     (.$irrigation & .$draining & lag$accum_rain[ord] > 0 & .$petp < 0) *
     pmin(.$lag_accum_rain_cm + .$petp_cm, 0)
 
-  .$corrected <- isTRUE(.$lag_accum_drain > 0 & (!.$draining | .$irrigation))
+  .$condition <- (.$lag_accum_drain > 0 & (!.$draining | .$irrigation))
 
-  idxs <- cumsum(.$corrected) > 0
+  idxs <- cumsum(isTRUE(.$condition)) > 0
   .$irrigation[idxs] <- lag$irrigation[ord][idxs]
   .$draining[idxs] <- lag$draining[ord][idxs]
   .$height_diff_cm[idxs] <- lag$height_diff_cm[ord][idxs]
 
-  return(.)
-}
+  .$corrected <- isTRUE(.$condition)
 
-compute_accum_pt1 <- function(.) {
   .$inflow <- .$irrigation * (
     .$draining * 5 + (1-.$draining) * (.$height_diff_cm - .$petp_cm)
   )
@@ -43,9 +91,8 @@ compute_accum_pt1 <- function(.) {
   .$outflow_drain <-
     .$corrected * .$lag_accum_drain + (1 - .$corrected) * .$outflow_drain
   .$outflow_rain <- .$outflow_rain + .$lag_accum_rain
-
-  .
 }
+
 compute_real_outflows <- function(.) {
   ### Emptying clusters
   n_clusters <- length(.$cluster_id)
