c20baa525912796cb8c985be90ba30ccbec1dac9
Valerio Gherardi <vgherard840@gmail.com>
2024-07-19 08:51:26 +0200

    hb_local_algorithm: general code cosmetics + add cluster sorting by env. variable

--- R/hb_local_algorithm.R
+++ R/hb_local_algorithm.R
@@ -37,60 +37,15 @@ compute_accum_pt1 <- function(., ideal_flow_rate = 5) {
                       pmax(.$inflow + .$petp_cm - .$height_diff_cm, 0)
   )
   .$outflow_m3 <- .$outflow * .$area / 100 / erahumed:::s_per_day()
-  .$outflow_rain <- (.$petp > 0) * .$petp * .$area / 1000 / erahumed:::s_per_day()
+  .$outflow_rain <- (.$petp < 0) * .$petp_m3_s
   .$outflow_phys <- .$draining * (.$outflow_m3 - .$outflow_rain)
   .$outflow_flux <- .$irrigation * .$outflow_phys
-  .$outflow_drain <- (1-.$irrigation) * .$outflow_phys
+  .$outflow_drain <- (1 - .$irrigation) * .$outflow_phys
   .$outflow_drain <-
     .$corrected * .$lag_accum_drain + (1 - .$corrected) * .$outflow_drain
   .$outflow_rain <- .$outflow_rain + .$lag_accum_rain
 
   .
-}
-
-compute_ideal_outflows <- function(., lag) {
-  ord <- match(.$cluster_id, .lag$cluster_id)
-
-  .$lag_accum_drain <- lag$accum_drain[ord]
-  .$lag_accum_drain <- pmax(
-    .$lag_accum_drain + (.$petp < 0 & .$lag_accum_drain > 0) * .$petp_m3_s,
-    0)
-
-  .$lag_accum_rain <- lag$accum_rain[ord]
-  .$lag_accum_rain <- .$lag_accum_rain +
-    (.$petp < 0 & .$lag_accum_rain > 0 & .$lag_accum_drain <= 0) * .$petp_m3_s
-
-  .$lag_accum_rain_cm <- .$lag_accum_rain * 100 * erahumed:::s_per_day() / .$area
-  .$lag_accum_rain <- pmax(.$lag_accum_rain, 0)
-
-  .$Evap_mismatch <-
-    (.$irrigation & .$draining & lag$accum_rain[ord] > 0 & .$petp < 0) *
-    pmin(.$lag_accum_rain_cm + .$petp_cm, 0)
-
-  .$condition <- (.$lag_accum_drain > 0 & (!.$draining | .$irrigation))
-
-  idxs <- cumsum(isTRUE(.$condition)) > 0
-  .$irrigation[idxs] <- lag$irrigation[ord][idxs]
-  .$draining[idxs] <- lag$draining[ord][idxs]
-  .$height_diff_cm[idxs] <- lag$height_diff_cm[ord][idxs]
-
-  .$corrected <- isTRUE(.$condition)
-
-  .$inflow <- .$irrigation * (
-    .$draining * 5 + (1-.$draining) * (.$height_diff_cm - .$petp_cm)
-  )
-  .$outflow <- ifelse(.$lag_accum_drain > 0 & .$petp < 0,
-                      pmax(.$inflow - .$height_diff_cm, 0),
-                      pmax(.$inflow + .$petp_cm - .$height_diff_cm, 0)
-  )
-  .$outflow_m3 <- .$outflow * .$area / 100 / erahumed:::s_per_day()
-  .$outflow_rain <- (.$petp > 0) * .$petp * .$area / 1000 / erahumed:::s_per_day()
-  .$outflow_phys <- .$draining * (.$outflow_m3 - .$outflow_rain)
-  .$outflow_flux <- .$irrigation * .$outflow_phys
-  .$outflow_drain <- (1-.$irrigation) * .$outflow_phys
-  .$outflow_drain <-
-    .$corrected * .$lag_accum_drain + (1 - .$corrected) * .$outflow_drain
-  .$outflow_rain <- .$outflow_rain + .$lag_accum_rain
 }
 
 compute_real_outflows <- function(.) {
@@ -99,7 +54,9 @@ compute_real_outflows <- function(.) {
   # Permute the rows of ., so that clusters in the same ditch
   # are emptied in a random order every day
 
-  sorted_clusters <- sample(n_clusters)
+  # Used for testing purposes
+  .order <- Sys.getenv("erahumed_hb_sort_clusters", "FALSE") |> as.logical()
+  sorted_clusters <- if (.order) order(.$cluster_id) else sample(n_clusters)
 
   capacity <- .$flowpoint[1]  # Extract this value outside of FOR
 
@@ -113,7 +70,6 @@ compute_real_outflows <- function(.) {
 
   cum_flows <- pmin(cumsum(c(0, .$outflow_flux[sorted_clusters])), capacity)
   .$real_outflow_flux[sorted_clusters] <- diff(cum_flows)
-  #capacity <- capacity - cum_flows[length(cum_flows)]
 
   .$outflow_m3 <- .$real_outflow_rain + .$real_outflow_drain + .$real_outflow_flux
   .$outflow <- .$outflow_m3 * erahumed:::s_per_day() * 100 / .$area
