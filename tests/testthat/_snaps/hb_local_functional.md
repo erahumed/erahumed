dd8fb9fb1c37f81796a98174128424f605b442d6
Valerio Gherardi <vgherard840@gmail.com>
2024-07-22 12:26:50 +0200

    Update tests to re-use data-frame + extend date range

--- tests/testthat/_snaps/hb_local_functional.md
+++ tests/testthat/_snaps/hb_local_functional.md
@@ -3,5 +3,5 @@
     Code
       hash
     Output
-      [1] "8e4984864e33052ca99d441b1195acf7"
+      [1] "4549e1cd347365d0188fb8a01d5bb796"
 
--- tests/testthat/test-hb_local_functional.R
+++ tests/testthat/test-hb_local_functional.R
@@ -1,12 +1,16 @@
+withr::with_envvar(
+  c(erahumed_hb_sort_clusters = TRUE),
+  test_df <- albufera_hydro_balance_local(
+    date_min = "2010-01-01", date_max = "2011-12-31")
+  )
+
+
 test_that("sum of cluster outflows does never exceed total ditch outflow", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
 
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  res <- test |>
+  res <- test_df |>
     dplyr::group_by(date, ditch) |>
     dplyr::summarise(
       flowpoint = flowpoint[1], outflow = sum(real_outflow_m3_s), .groups = "drop"
@@ -20,10 +24,7 @@ test_that("real outflows never exceed ideal ones", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  res <- test |>
+  res <- test_df |>
     dplyr::filter(
       real_outflow_rain > ideal_outflow_rain + tol |
       real_outflow_drain > ideal_outflow_drain + tol |
@@ -37,10 +38,7 @@ test_that("drain and flux (real) outflows are always non-negative", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  res <- test |>
+  res <- test_df |>
     dplyr::filter(real_outflow_drain < 0 | real_outflow_flux < 0)
 
   expect_equal(nrow(res), 0)
@@ -50,10 +48,7 @@ test_that("rain outflow is always non-negative", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  res <- test |>
+  res <- test_df |>
     dplyr::filter(real_outflow_rain < 0)
 
   skip("Rain outflow can actually become negative (with small values)") # TODO
@@ -64,10 +59,7 @@ test_that("inflow is zero if not irrigating", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  res <- test |>
+  res <- test_df |>
     dplyr::filter(!irrigation, abs(inflow) / mean(abs(inflow)) > tol)
 
   expect_equal(nrow(res), 0)
@@ -77,10 +69,7 @@ test_that("accum_drain is greater than zero only if draining", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  res <- test |>
+  res <- test_df |>
     dplyr::filter(accum_drain > 0, !draining)
 
   expect_equal(nrow(res), 0)
@@ -91,10 +80,7 @@ test_that("simple snapshot is constant", {
 
   tol <- 1e-10
 
-  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
-                                       date_max = "2010-02-01")
-
-  hash <- digest::digest(test)
+  hash <- digest::digest(test_df)
 
   expect_snapshot(hash)
 })
