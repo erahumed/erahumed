a35f8eff33821aa9ba885163009219f799282034
Valerio Gherardi <vgherard840@gmail.com>
2024-07-22 09:32:33 +0200

    test_that("inflow is zero if not irrigating")

--- tests/testthat/test-hb_local_functional.R
+++ tests/testthat/test-hb_local_functional.R
@@ -60,6 +60,19 @@ test_that("rain outflow is always non-negative", {
   expect_equal(nrow(res), 0)
 })
 
+test_that("inflow is zero if not irrigating", {
+  withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
+  tol <- 1e-10
+
+  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
+                                       date_max = "2010-02-01")
+
+  res <- test |>
+    dplyr::filter(!irrigation, abs(inflow) / mean(abs(inflow)) > tol)
+
+  expect_equal(nrow(res), 0)
+})
+
 test_that("simple snapshot is constant", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
 
