f36515cf9270c919c35432bf94629428a15c6724
Valerio Gherardi <vgherard840@gmail.com>
2024-07-22 12:10:46 +0200

    Add test

--- tests/testthat/test-hb_local_functional.R
+++ tests/testthat/test-hb_local_functional.R
@@ -73,6 +73,19 @@ test_that("inflow is zero if not irrigating", {
   expect_equal(nrow(res), 0)
 })
 
+test_that("accum_drain is greater than zero only if draining", {
+  withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
+  tol <- 1e-10
+
+  test <- albufera_hydro_balance_local(date_min = "2010-01-01",
+                                       date_max = "2010-02-01")
+
+  res <- test |>
+    dplyr::filter(accum_drain > 0, !draining)
+
+  expect_equal(nrow(res), 0)
+})
+
 test_that("simple snapshot is constant", {
   withr::local_envvar(erahumed_hb_sort_clusters = TRUE)
 
