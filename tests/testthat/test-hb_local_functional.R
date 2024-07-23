e1e76a69510c9fcfdb22279c1a2be1d3a2eb8b26
Valerio Gherardi <vgherard840@gmail.com>
2024-07-23 09:30:42 +0200

    Add tests on height_diff_cm output column

--- tests/testthat/test-hb_local_functional.R
+++ tests/testthat/test-hb_local_functional.R
@@ -1,8 +1,70 @@
 withr::with_envvar(c(erahumed_hb_sort_clusters = TRUE), {
   test_df <- albufera_hydro_balance_local(date_min = "2010-01-01",
                                           date_max = "2011-12-31")
-  tol <- 1e-10
+  eps <- 1e-10
   })
+
+
+
+test_that("simple snapshot is constant", {
+  hash <- digest::digest(test_df)
+
+  expect_snapshot(hash)
+})
+
+
+
+test_that("If draining, the ideal height difference is non-positive", {
+  res <- test_df |>
+    dplyr::filter(draining, height_diff_cm > 1 * eps)
+
+  expect_equal(nrow(res), 0)
+})
+
+test_that("If irrigating, the ideal height difference is non-negative", {
+  res <- test_df |>
+    dplyr::filter(irrigation, height_diff_cm < -1 * eps)
+
+  expect_equal(nrow(res), 0)
+})
+
+test_that("If draining and irrigating, the ideal height difference is zero", {
+  res <- test_df |>
+    dplyr::filter(draining, irrigation, abs(height_diff_cm) > 1 * eps)
+
+  expect_equal(nrow(res), 0)
+})
+
+
+
+test_that("If draining, ideal inflow > ideal height difference", {
+  res <- test_df |>
+    dplyr::filter(draining,
+                  height_diff_cm - ideal_inflow_cm > mean(abs(ideal_inflow_cm)) * eps
+                  )
+
+  expect_equal(nrow(res), 0)
+})
+
+test_that("ideal inflow is zero if not irrigating", {
+  res <- test_df |>
+    dplyr::filter(!irrigation,
+                  abs(ideal_inflow_cm) / mean(abs(ideal_inflow_cm)) > eps
+    )
+
+  expect_equal(nrow(res), 0)
+})
+
+test_that("real inflow is zero if not irrigating", {
+  res <- test_df |>
+    dplyr::filter(!irrigation,
+                  abs(real_inflow_cm) / mean(abs(real_inflow_cm)) > eps
+                  )
+
+  skip("Inflow can actually be != 0 if Evap_mismatch != 0") # TODO
+  expect_equal(nrow(res), 0)
+})
+
 
 
 test_that("outflows are always non-negative", {
@@ -16,55 +78,29 @@ test_that("outflows are always non-negative", {
   expect_equal(nrow(res), 0)
 })
 
+test_that("real outflows never exceed their ideal counterparts", {
+  res <- test_df |>
+    dplyr::filter(
+      real_outflow_rain > ideal_outflow_rain + eps |
+      real_outflow_drain > ideal_outflow_drain + eps |
+      real_outflow_flux > ideal_outflow_flux + eps
+      )
+
+  expect_equal(nrow(res), 0)
+})
+
 test_that("sum of cluster outflows does never exceed total ditch outflow", {
   res <- test_df |>
     dplyr::group_by(date, ditch) |>
     dplyr::summarise(
       flowpoint = flowpoint[1], outflow = sum(real_outflow_m3_s), .groups = "drop"
       ) |>
-    dplyr::filter(outflow > flowpoint + tol)
+    dplyr::filter(outflow > flowpoint + eps)
 
   expect_equal(nrow(res), 0)
   })
 
-test_that("real outflows never exceed their ideal counterparts", {
-  res <- test_df |>
-    dplyr::filter(
-      real_outflow_rain > ideal_outflow_rain + tol |
-      real_outflow_drain > ideal_outflow_drain + tol |
-      real_outflow_flux > ideal_outflow_flux + tol
-      )
 
-  expect_equal(nrow(res), 0)
-})
-
-test_that("ideal inflow is never greater than ideal height difference", {
-  res <- test_df |>
-    dplyr::filter(draining,
-                  height_diff_cm - ideal_inflow_cm > mean(abs(ideal_inflow_cm)) * tol
-                  )
-
-  expect_equal(nrow(res), 0)
-})
-
-test_that("ideal inflow is zero if not irrigating", {
-  res <- test_df |>
-    dplyr::filter(!irrigation,
-                  abs(ideal_inflow_cm) / mean(abs(ideal_inflow_cm)) > tol
-    )
-
-  expect_equal(nrow(res), 0)
-})
-
-test_that("real inflow is zero if not irrigating", {
-  res <- test_df |>
-    dplyr::filter(!irrigation,
-                  abs(real_inflow_cm) / mean(abs(real_inflow_cm)) > tol
-                  )
-
-  skip("Inflow can actually be != 0 if Evap_mismatch != 0") # TODO
-  expect_equal(nrow(res), 0)
-})
 
 test_that("If ideal_outflow_drain > 0 the cluster must be draining", {
   res <- test_df |>
@@ -80,8 +116,3 @@ test_that("If accum_drain is greater than zero the cluster must be draining", {
   expect_equal(nrow(res), 0)
 })
 
-test_that("simple snapshot is constant", {
-  hash <- digest::digest(test_df)
-
-  expect_snapshot(hash)
-})
