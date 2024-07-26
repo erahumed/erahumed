33e3fe5ae9da623b7b1f93f323cfd9320accc662
Valerio Gherardi <vgherard840@gmail.com>
2024-07-26 13:35:38 +0200

    Remove non-ASCII string from spatial metadata

--- data-raw/albufera_clusters.R
+++ data-raw/albufera_clusters.R
@@ -7,6 +7,11 @@ clusters_raw <- sf::st_read("data-raw/raw/paddysdef.shp") |>
 albufera_cluster_geometries <- clusters_raw |>
   select(cluster_id, geometry)
 
+# Remove non-ASCII string (degree symbol) from metadata
+crs <- sf::st_crs(albufera_cluster_geometries$geometry)
+crs$wkt <- iconv(crs$wkt, from = "UTF-8", to = "ASCII//TRANSLIT")
+sf::st_crs(albufera_cluster_geometries$geometry) <- crs
+
 albufera_clusters <- sf::st_drop_geometry(clusters_raw) |>
   transmute(
     cluster_id,
@@ -16,6 +21,8 @@ albufera_clusters <- sf::st_drop_geometry(clusters_raw) |>
     rice_variety = mngmt,
     case = gsub("Case", "", CASE) |> as.factor()
   )
+
+
 
 usethis::use_data(albufera_clusters, overwrite = TRUE)
 usethis::use_data(albufera_cluster_geometries, overwrite = TRUE)
