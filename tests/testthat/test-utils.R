cf14191d1c30470ac9a4666a7a3520e936478db3
Valerio Gherardi <vgherard840@gmail.com>
2024-07-30 13:07:08 +0200

    Test unit conversion helpers

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -56,3 +56,20 @@ test_that("get_mm() and get_dd() returns the correct values in simple cases", {
   expect_equal(mm, c(1, 12, 1, 10))
   expect_equal(dd, c(1, 31, 24, 2))
 })
+
+
+test_that("cm_day_to_m3_s() returns correct value in simple case", {
+  input_cm <- 1 * 100  # 1 m3 of water in 1 day
+  area_m2 <- 1
+
+  expected <- 1 / s_per_day()
+  expect_equal(cm_day_to_m3_s(input_cm, area_m2), expected)
+})
+
+test_that("unit conversion helpers return correct values in simple cases", {
+  input_m3_s <- 1  # 1 m3/s of water during 1 day
+  area_m2 <- 1
+
+  expected <- 100 * s_per_day()
+  expect_equal(m3_s_to_cm_day(input_m3_s, area_m2), expected)
+})
