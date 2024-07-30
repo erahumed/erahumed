5c1f9642ff2b620f89c6647cd5f5ece1e4d80121
Valerio Gherardi <vgherard840@gmail.com>
2024-07-30 13:02:08 +0200

    Add unit conversion helpers

--- R/utils.R
+++ R/utils.R
@@ -28,3 +28,11 @@ get_dd <- function(date) {
 s_per_day <- function() {
   24 * 60 * 60
 }
+
+cm_day_to_m3_s <- function(x, area_m2) {
+  x * area_m2 / s_per_day() / 100
+}
+
+m3_s_to_cm_day <- function(x, area_m2) {
+  x * 100 * s_per_day() / area_m2
+}
