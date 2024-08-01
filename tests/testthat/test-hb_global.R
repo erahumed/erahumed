e1cbbc735313530a88acf77be6d25e8d009a1ce6
Valerio Gherardi <vgherard840@gmail.com>
2024-08-01 13:47:01 +0200

    Test that hb_global() returns an object of the correct class

---
+++ tests/testthat/test-hb_global.R
@@ -0,0 +1,8 @@
+test_that("multiplication works", {
+  obj <-  hb_global(level = rep(1, 10),
+                    P = rep(10, 10),
+                    ETP = rep(1, 10),
+                    outflows = list(a = rep(1, 10), b = rep(2, 10))
+                    )
+  expect_s3_class(obj, "hb_global")
+})
