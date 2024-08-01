52ac59dab82020b0c25cf9f286ab0f5e8170139f
Valerio Gherardi <vgherard840@gmail.com>
2024-08-01 13:39:29 +0200

    More strict input checks in make_hb_global()

--- R/hb_global.R
+++ R/hb_global.R
@@ -90,8 +90,18 @@ hb_global <- function(
 
 
 make_hb_global <- function(df) {
+  name <- deparse(substitute(df))
+
   if ( !("data.frame" %in% class(df)) )
-    stop( paste(deparse(substitute(df)), "must be a 'data.frame'.") )
+    stop( paste(name, "must be a 'data.frame'.") )
+
+  # Check that df has the required columns
+  required_cols <- c("level", "P", "ETP", "volume",
+                     "inflow_total", "outflow_total", "residence_time_days")
+  for (col in required_cols) {
+    if (col %in% colnames(df)) next
+    stop(paste0("Missing '", col, "' column in input '", name, "'."))
+  }
 
   class(df) <- c("hb_global", "data.frame")
   attr(class(df), "package") <- "erahumed"
