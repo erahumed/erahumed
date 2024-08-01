9751e6ee2e91951d0696137fa8f85ab9bfd33c88
Valerio Gherardi <vgherard840@gmail.com>
2024-08-01 14:13:52 +0200

    Move make_hb_global() to hb_global_s3_class.R

--- R/hb_global.R
+++ R/hb_global.R
@@ -101,29 +101,3 @@ hb_global_check_args <- function(level, P, ETP, outflows, ...) {
   if (length( unique(lengths) ) > 1)
     stop("Time series inputs must have equal lengths, see ?hb_global.")
 }
-
-
-
-make_hb_global <- function(df) {
-  name <- deparse(substitute(df))
-
-  if ( !("data.frame" %in% class(df)) )
-    stop( paste(name, "must be a 'data.frame'.") )
-
-  # Check that df has the required columns
-  required_cols <- c("level", "P", "ETP", "volume",
-                     "inflow_total", "outflow_total", "residence_time_days")
-  for (col in required_cols) {
-    if (col %in% colnames(df)) next
-    stop(paste0("Missing '", col, "' column in input '", name, "'."))
-  }
-
-  class(df) <- c("hb_global", class(df))
-  attr(class(df), "package") <- "erahumed"
-  return(df)
-}
-
-
-
-
-
---
+++ R/hb_global_s3_class.R
@@ -0,0 +1,18 @@
+make_hb_global <- function(df) {
+  name <- deparse(substitute(df))
+
+  if ( !("data.frame" %in% class(df)) )
+    stop( paste(name, "must be a 'data.frame'.") )
+
+  # Check that df has the required columns
+  required_cols <- c("level", "P", "ETP", "volume",
+                     "inflow_total", "outflow_total", "residence_time_days")
+  for (col in required_cols) {
+    if (col %in% colnames(df)) next
+    stop(paste0("Missing '", col, "' column in input '", name, "'."))
+  }
+
+  class(df) <- c("hb_global", class(df))
+  attr(class(df), "package") <- "erahumed"
+  return(df)
+}
