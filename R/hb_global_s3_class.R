00c7e28f9fcc131c5505ff3a3700e0527ff7e969
Valerio Gherardi <vgherard840@gmail.com>
2024-08-02 14:44:16 +0200

    Ensure make_hb_global() raises errors of the correct class

--- R/hb_global_s3_class.R
+++ R/hb_global_s3_class.R
@@ -1,15 +1,22 @@
-make_hb_global <- function(df) {
-  name <- deparse(substitute(df))
+make_hb_global <- function(df)
+{
+  .stop <- function(message)
+    stop(structure(list(message = message),
+                   class = c("make_hb_global_error", "error", "condition")
+                   )
+         )
 
-  if ( !("data.frame" %in% class(df)) )
-    stop( paste(name, "must be a 'data.frame'.") )
+  name <- deparse(substitute(df))
+  if ( !("data.frame" %in% class(df)) ) {
+    .stop( paste(name, "must be a 'data.frame'.") )
+  }
 
   # Check that df has the required columns
   required_cols <- c("level", "rain_mm", "evapotranspiration_mm", "volume",
                      "inflow_total", "outflow_total", "residence_time_days")
   for (col in required_cols) {
     if (col %in% colnames(df)) next
-    stop(paste0("Missing '", col, "' column in input '", name, "'."))
+    .stop(paste0("Missing '", col, "' column in input '", name, "'."))
   }
 
   class(df) <- c("hb_global", class(df))
---
+++ tests/testthat/test-hb_global_s3_class.R
@@ -0,0 +1,8 @@
+test_that("Ensure constructor raises errors of the correct class", {
+
+  expect_error(make_hb_global("not a df"), class = "make_hb_global_error")
+
+  expect_error(make_hb_global(data.frame(x = 1:10, y = 10:1)),  # Wrong cols
+               class = "make_hb_global_error")
+
+})
