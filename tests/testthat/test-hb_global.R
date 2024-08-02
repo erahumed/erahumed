e0a25f00ce253b2eedef58cacc055c98d3a22984
Valerio Gherardi <vgherard840@gmail.com>
2024-08-02 09:23:26 +0200

    Test that hb_global() does not return any NA values

--- tests/testthat/test-hb_global.R
+++ tests/testthat/test-hb_global.R
@@ -6,3 +6,13 @@ test_that("hb_global() succeeds with valid inputs", {
                     )
   expect_s3_class(obj, "hb_global")
 })
+
+test_that("hb_global() has no NA values", {
+  obj <-  hb_global(level = rep(1, 10),
+                    P = rep(10, 10),
+                    ETP = rep(1, 10),
+                    outflows = list(a = rep(1, 10), b = rep(2, 10))
+                    )
+
+  expect_no_error(na.fail(obj))
+})
