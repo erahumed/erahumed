969b9bb46cf1ef98e4965f25648f51fda7378533
Valerio Gherardi <vgherard840@gmail.com>
2024-08-02 12:31:32 +0200

    Test exceptions raised by hb_global() (Close #24)

--- tests/testthat/test-hb_global.R
+++ tests/testthat/test-hb_global.R
@@ -7,7 +7,18 @@ test_that("hb_global() succeeds with valid inputs", {
   expect_s3_class(obj, "hb_global")
 })
 
-test_that("hb_global() has no NA values", {
+test_that("hb_global() succeeds with ellipsis args of correct length", {
+  expect_no_error(hb_global(level = rep(1, 10),
+                            P = 1:10,
+                            ETP = rep(1, 10),
+                            outflows = list(a = rep(1, 10), b = rep(2, 10)),
+                            correct_length_vec = 1:10,
+                            another_correct_length_vec = letters[1:10]
+                            )
+                  )
+})
+
+test_that("hb_global() output has no NA values", {
   obj <-  hb_global(level = rep(1, 10),
                     P = rep(10, 10),
                     ETP = rep(1, 10),
@@ -27,9 +38,42 @@ test_that("hb_global() raises an error if inputs have wrong lengths", {
 
 test_that("hb_global() raises an error if inputs have wrong type", {
   expect_error(hb_global(level = rep(1, 10),
-                         P = rep(10, 5),
+                         P = letters[1:10],
                          ETP = rep(1, 10),
                          outflows = list(a = rep(1, 10), b = rep(2, 10))),
                class = "hb_global_argcheck_error"
+  )
+})
+
+test_that("hb_global() error if storage_curve() has invalid signature", {
+  expect_error(hb_global(level = rep(1, 10),
+                         P = 1:10,
+                         ETP = rep(1, 10),
+                         outflows = list(a = rep(1, 10), b = rep(2, 10)),
+                         storage_curve = function() 1
+                         ),
+               class = "hb_global_argcheck_error"
+  )
+})
+
+test_that("hb_global() error if petp_surface() has invalid signature", {
+  expect_error(hb_global(level = rep(1, 10),
+                         P = 1:10,
+                         ETP = rep(1, 10),
+                         outflows = list(a = rep(1, 10), b = rep(2, 10)),
+                         petp_surface = function(x) x
+  ),
+  class = "hb_global_argcheck_error"
+  )
+})
+
+test_that("hb_global() error if an ellipsis arg has the wrong length", {
+  expect_error(hb_global(level = rep(1, 10),
+                         P = 1:10,
+                         ETP = rep(1, 10),
+                         outflows = list(a = rep(1, 10), b = rep(2, 10)),
+                         wrong_length_vec = 1:9
+                         ),
+  class = "hb_global_argcheck_error"
   )
 })
