4c47e9c56f7f6fb0437035404a578ba90a51cb92
Valerio Gherardi <vgherard840@gmail.com>
2024-08-05 14:21:53 +0200

    Separate HB global and local wrappers

---
+++ R/hb_global_wrapper.R
@@ -0,0 +1,53 @@
+#' Albufera Hydrological Balance
+#'
+#' Documentation TBD
+#'
+#' @details
+#' The numeric inputs used from the linear storage curve and P-ETP surface were
+#' extracted from the CHJ report
+#' [Modelo de seguimiento de l’Albufera de Valencia con AQUATOOLDMA.](https://www.chj.es/Descargas/ProyectosOPH/Consulta%20publica/PHC-2015-2021/ReferenciasBibliograficas/HumedalesZonasProtegidas/CHJ,2012.Aquatool_Albufera.pdf)
+#'
+#'
+#' @export
+albufera_hb_global <- function(
+    outflows_df = erahumed::albufera_outflows,
+    petp_df = erahumed::albufera_petp,
+    storage_curve = linear_storage_curve(intercept = 16.7459 * 1e6,
+                                         slope = 23.6577 * 1e6),
+    petp_surface = linear_petp_surface(surface_P = 114.225826072 * 1e6,
+                                       surface_ETP = 79.360993685 * 1e6)
+)
+{
+  albufera_hb_global_datacheck(outflows_df, petp_df)
+
+  # Just to get intersection of dates
+  input <- merge(outflows_df, petp_df, by = "date", sort = TRUE)
+
+  hb_global(
+    level = input$level,
+    rain_mm = input$rain_mm,
+    evapotranspiration_mm = input$evapotranspiration_mm,
+    outflows = data.frame(pujol = input$pujol,
+                          perellonet = input$perellonet,
+                          perello = input$perello),
+    date = input$date,
+    level_is_imputed = input$level_is_imputed,
+    outflow_is_imputed = input$outflow_is_imputed,
+    storage_curve = storage_curve,
+    petp_surface = petp_surface
+  )
+}
+
+albufera_hb_global_datacheck <- function(outflows_df, petp_df) {
+  tryCatch(
+    {
+      assert_data.frame(outflows_df, template = erahumed::albufera_outflows)
+      assert_data.frame(petp_df, template = erahumed::albufera_petp)
+    },
+    error = function(e) {
+      class(e) <- c("albufera_hb_global_datacheck_error", class(e))
+      stop(e)
+    }
+  )
+
+}
