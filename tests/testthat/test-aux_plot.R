cf10fbca59a92586d3419cc44cdbe7a6a1f5ead3
Valerio Gherardi <vgherard840@gmail.com>
2024-08-06 11:01:35 +0200

    Test that plotting utilities succeed with default arguments

---
+++ tests/testthat/test-aux_plot.R
@@ -0,0 +1,7 @@
+test_that("plot_albufera_clusters(): no error with default args", {
+  expect_no_error(plot_albufera_clusters())
+})
+
+test_that("plot_albufera_management(): no error with default args", {
+  expect_no_error(plot_albufera_management())
+})
