28e40455324bc2be9d3498d2d0907359b2ce0d7b
Valerio Gherardi <vgherard840@gmail.com>
2024-08-06 12:22:51 +0200

    Test hb_shiny components (close #15)

--- tests/testthat/test-hb_shiny.R
+++ tests/testthat/test-hb_shiny.R
@@ -6,9 +6,37 @@ testServer(hbGlobalServer, {
     variable = "level"
     )
 
-  d <- hb_data()
-  expect_equal(min(d$date), input$date_range[1])
-  expect_equal(max(d$date), input$date_range[2])
+  expect_equal(min(hb_data()$date), input$date_range[1])
+  expect_equal(max(hb_data()$date), input$date_range[2])
 
   output$hb_plot
+})
+
+testServer(hbLocalServer, {
+
+  output$albufera_map  # Plotted without error (this line takes ~3s)
+  expect_null(hb_data())
+
+  session$setInputs(date_range = as.Date(c("2013-01-01", "2013-01-31")))
+
+  expect_null(hb_data())  # Still NULL when only date range is set
+
+  session$setInputs(cluster_id = albufera_clusters$cluster_id[1])
+
+  expect_null(hb_data())  # Still NULL because not yet ran
+
+  session$setInputs(run_button = 1)
+
+  expect_s3_class(hb_data(), "hb_local")  # Still NULL because not yet ran
+
+  expect_equal(min(hb_data()$date), input$date_range[1])
+  expect_equal(max(hb_data()$date), input$date_range[2])
+
+  output$hb_plot
+
+  skip("Testing click interaction of leaflet not currently implemented.")
+  session$setInputs(albufera_map_shape_click = list(id = "NewpointB2-98_5"))
+
+  expect_equal(input$cluster_id, input$albufera_map_shape_click$id)
+
 })
