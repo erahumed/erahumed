caf778d34eaaed231fcd7f0dfbf518c9d0f0c9f6
Valerio Gherardi <vgherard840@gmail.com>
2024-08-06 14:02:33 +0200

    Fix hb_shiny tests

--- tests/testthat/test-hb_shiny.R
+++ tests/testthat/test-hb_shiny.R
@@ -3,6 +3,7 @@ library(shiny)
 test_that("UIs are return shiny objects without errors", {
 
   expect_no_error(hbUI("id"))
+  expect_no_error(hbSetupUI("id"))
   expect_no_error(hbGlobalUI("id"))
   expect_no_error(hbLocalUI("id"))
 
@@ -10,36 +11,31 @@ test_that("UIs are return shiny objects without errors", {
 
 
 testServer(hbGlobalServer, {
-  session$setInputs(
-    date_range = as.Date(c("2013-01-01", "2014-12-31")),
-    variable = "level"
-    )
+  session$setInputs(variable = "level")
 
-  expect_equal(min(hb_data()$date), input$date_range[1])
-  expect_equal(max(hb_data()$date), input$date_range[2])
+  expect_equal(min(hb_data()$date), setup$date_range[1])
+  expect_equal(max(hb_data()$date), setup$date_range[2])
 
   output$hb_plot
-})
+  },
+  args = list(
+    setup = list(date_range = as.Date(c("2013-01-01", "2014-12-31")))
+    )
+  )
 
 testServer(hbLocalServer, {
 
   output$albufera_map  # Plotted without error (this line takes ~3s)
   expect_null(hb_data())
 
-  session$setInputs(date_range = as.Date(c("2013-01-01", "2013-01-31")))
-
-  expect_null(hb_data())  # Still NULL when only date range is set
-
   session$setInputs(cluster_id = albufera_clusters$cluster_id[1])
-
   expect_null(hb_data())  # Still NULL because not yet ran
 
   session$setInputs(run_button = 1)
+  expect_s3_class(hb_data(), "hb_local")
 
-  expect_s3_class(hb_data(), "hb_local")  # Still NULL because not yet ran
-
-  expect_equal(min(hb_data()$date), input$date_range[1])
-  expect_equal(max(hb_data()$date), input$date_range[2])
+  expect_equal(min(hb_data()$date), setup$date_range[1])
+  expect_equal(max(hb_data()$date), setup$date_range[2])
 
   output$hb_plot
 
@@ -48,4 +44,10 @@ testServer(hbLocalServer, {
 
   expect_equal(input$cluster_id, input$albufera_map_shape_click$id)
 
-})
+  },
+  args = list(
+    setup = list(date_range = as.Date(c("2013-01-01", "2013-01-10")),
+                 ideal_flow_rate_cm = 5
+                 )
+    )
+  )
