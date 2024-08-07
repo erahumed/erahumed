447b74a83b371b39e7b2ac61acb268f1f564360c
Valerio Gherardi <vgherard840@gmail.com>
2024-08-07 08:19:47 +0200

    Add basic test with shinytest2

--- tests/testthat/setup-shinytest2.R
+++ tests/testthat/setup-shinytest2.R
@@ -1,2 +1,3 @@
-# Load application support files into testing environment
-shinytest2::load_app_env()
+# Do not load code via `load_app_env`
+# This is a workaround for the warnings otherwise raised by shinytest2. See
+# https://github.com/rstudio/shinytest2/issues/264#issuecomment-1621936984
