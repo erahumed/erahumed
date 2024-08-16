db3cd5a920881ff80f31dc1a1bbd9cf6249d9428
Valerio Gherardi <vgherard840@gmail.com>
2024-08-16 12:58:09 +0200

    Remove plot_albufera_management() unused helper, that was causing annoying r-cmd-check notes

--- tests/testthat/test-aux_plot.R
+++ tests/testthat/test-aux_plot.R
@@ -1,7 +1,3 @@
 test_that("plot_albufera_clusters(): no error with default args", {
   expect_no_error(plot_albufera_clusters())
 })
-
-test_that("plot_albufera_management(): no error with default args", {
-  expect_no_error(plot_albufera_management())
-})
