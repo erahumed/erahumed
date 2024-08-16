8da860b8a57808bdb005b7fb2e083b2d18bb8c25
Valerio Gherardi <vgherard840@gmail.com>
2024-08-16 11:48:32 +0200

    Remove tests covered by test-hb_global_units.R

--- tests/testthat/test-hb_global_wrapper.R
+++ tests/testthat/test-hb_global_wrapper.R
@@ -14,35 +14,3 @@ test_that("albufera_hb_global(): errors if provided with invalid input data", {
     class = "albufera_hb_global_datacheck_error"
     )
 })
-
-
-
-# TODO: These tests are really tests on the lower level logical units... move?
-test_that("Hydrological balance checks out", {
-  tol <- 1e-12
-
-  df <- albufera_hb_global()
-
-  res <- df |>
-    dplyr::mutate(
-      x = volume_change,
-      y = (inflow_total - outflow_total) * s_per_day() + volume_change_petp
-    ) |>
-    dplyr::filter(abs(x - y) / mean(abs(x)) > tol)
-
-  expect_equal(nrow(res), 0)
-  })
-
-test_that("outflow_extra != requires inflow_total == 0", {
-  tol <- 1e-12
-
-  df <- albufera_hb_global()
-
-  res <- df |>
-    dplyr::filter(abs(inflow_total) > tol * mean(abs(inflow_total)),
-                  abs(outflow_extra) > tol * mean(abs(outflow_total))
-                  )
-
-  expect_equal(nrow(res), 0)
-})
-
