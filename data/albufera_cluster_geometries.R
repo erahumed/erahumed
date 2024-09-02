b8cda156335c306f40cc04dca2f3fd21baf26938
Valerio Gherardi <vgherard840@gmail.com>
2024-09-02 12:06:04 +0200

    Generate dummy error when env var is set
    Former-commit-id: ea005dfef25a32eced162114f61d363bee172fd6

--- data/albufera_cluster_geometries.R
+++ data/albufera_cluster_geometries.R
@@ -1,4 +1,10 @@
 delayedAssign("albufera_cluster_geometries", local({
+  # Used to test that errors for this assignment still allow normal execution
+  # of other functions in the package
+  testing_env_path <- "erahumed_test_cluster_geometries_error"
+  is_testing_env <- Sys.getenv(testing_env_path) == "TRUE"
+  stopifnot(!is_testing_env)
+
   requireNamespace("sf", quietly = TRUE)
   try(erahumed:::albufera_cluster_geometries, silent = TRUE)
 }))
