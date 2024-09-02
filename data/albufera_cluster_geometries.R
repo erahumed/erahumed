bec144662129954d8711fdadc1e50888b444edb7
Valerio Gherardi <vgherard840@gmail.com>
2024-09-02 12:13:11 +0200

    Allow installation of package even if "erahumed_test_cluster_geometries_error" is set, to simulate actual behaviour
    Former-commit-id: 552f2d2aa9acbc2894a249f379d2f97ad6462e5c

--- data/albufera_cluster_geometries.R
+++ data/albufera_cluster_geometries.R
@@ -1,10 +1,12 @@
 delayedAssign("albufera_cluster_geometries", local({
-  # Used to test that errors for this assignment still allow normal execution
-  # of other functions in the package
-  testing_env_path <- "erahumed_test_cluster_geometries_error"
-  is_testing_env <- Sys.getenv(testing_env_path) == "TRUE"
-  stopifnot(!is_testing_env)
+  try({
+    # Simulate error, to test what would happen if the evaluation of
+    # erahumed:::albufera_cluster_geometries failed.
+    testing_env_path <- "erahumed_test_cluster_geometries_error"
+    is_testing_env <- Sys.getenv(testing_env_path) == "TRUE"
+    stopifnot(!is_testing_env)
 
-  requireNamespace("sf", quietly = TRUE)
-  try(erahumed:::albufera_cluster_geometries, silent = TRUE)
+    requireNamespace("sf", quietly = TRUE)
+    erahumed:::albufera_cluster_geometries
+  }, silent = TRUE)
 }))
