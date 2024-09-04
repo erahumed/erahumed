ef36497e86c0bbff3cbe51f2f24edeb253e63d21
Valerio Gherardi <vgherard840@gmail.com>
2024-09-04 13:33:24 +0200

    New intermediate wrapper ca_to_cluster_wrap()
    Former-commit-id: 2553170479a40561687da2943f06768e0318313f

--- R/ca.R
+++ R/ca.R
@@ -1,16 +1,3 @@
-ca_cluster <- function(date,
-                       real_height_cm,
-                       irrigation,
-                       draining,
-                       plan_delay,
-                       application_days,
-                       amounts,
-                       sowing_day = "04-20"
-                       )
-{
-  return(numeric(length(date)))  # TODO
-}
-
 ca <- function(hbl, ca_schedules_df = erahumed::albufera_ca_schedules)
 {
   stopifnot(inherits(hbl, "hb_local"))
@@ -22,29 +9,48 @@ ca <- function(hbl, ca_schedules_df = erahumed::albufera_ca_schedules)
       simplify = FALSE,
       keep.by = TRUE
     ) |>
-    lapply(\(df){
-
-      variety <- df$variety[[1]]
-      chemicals <- unique(df$chemicals)
-
-      FUN <- function(application_days, amounts) {
-        chem_apply_cluster(date = df$date,
-                           real_height_cm = df$real_height_cm,
-                           irrigation = df$irrigation,
-                           draining = df$draining,
-                           plan_delay = df$plan_delay,
-                           application_days = application_days,
-                           amounts = amounts)
-      }
-
-      for (chemical in chemicals) {
-        df1 <- df[df$variety == variety & df$chemical == chemical, ]
-        df[applications$chemical] <- FUN(application_days = df1$day,
-                                         amounts = df1$amount)
-      }
-
-      df
-    }) |>
+    lapply(ca_to_cluster_wrap, ca_schedules_df = ca_schedules_df) |>
     data.table::rbindlist() |>
     as.data.frame()
 }
+
+ca_to_cluster_wrap <- function(cluster_hbl_df, ca_schedules_df)
+{
+  variety <- cluster_hbl_df$variety[[1]]
+
+  ca_to_cluster_fun <- function(application_days, amounts) {
+    ca_to_cluster(date = cluster_hbl_df$date,
+                  real_height_cm = cluster_hbl_df$real_height_cm,
+                  irrigation = cluster_hbl_df$irrigation,
+                  draining = cluster_hbl_df$draining,
+                  plan_delay = cluster_hbl_df$plan_delay,
+                  application_days = application_days,
+                  amounts = amounts)
+  }
+
+  res <- cluster_hbl_df
+  for (chemical in unique(ca_schedules_df$chemical)) {
+    scdl_filtered <- ca_schedules_df |>
+      (\(.) .[.$variety == variety & .$chemical == chemical, ])()
+    res[chemical] <- ca_to_cluster_fun(application_days = scdl_filtered$day,
+                                       amounts = scdl_filtered$amount)
+  }
+
+  res
+}
+
+ca_to_cluster <- function(date,
+                          real_height_cm,
+                          irrigation,
+                          draining,
+                          plan_delay,
+                          application_days,
+                          amounts,
+                          sowing_day = "04-20")
+{
+  return(numeric(length(date)))  # TODO
+}
+
+
+
+
