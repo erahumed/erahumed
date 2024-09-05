b8f7ec6b51c3b3be304a21400632737932d6c9fe
Valerio Gherardi <vgherard840@gmail.com>
2024-09-05 11:56:09 +0200

    Add "plot_albufera_management" helper to sandbox/ scripts

---
+++ sandbox/plot_albufera_management.R
@@ -0,0 +1,16 @@
+plot_albufera_management <- function(management_df = albufera_management) {
+  management_df$date <- as.Date(paste("2000",
+                                      management_df$mm,
+                                      management_df$dd,
+                                      sep = "-"))
+
+  management_df |>
+    ggplot2::ggplot(ggplot2::aes(x = date, y = height_cm, color = tancat)) +
+    ggplot2::geom_line() +
+    ggplot2::facet_grid(variety ~ .) +
+    ggplot2::scale_x_date(date_labels = "%m/%d", date_breaks = "1 month") +
+    ggplot2::scale_color_manual(values = c("TRUE" = "blue", "FALSE" = "red")) +
+    ggplot2::labs(x = "Day of the Year",
+                  y = "Height (cm)",
+                  title = "Height vs Day of the Year")
+}
