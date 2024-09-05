bb995e73466066f6537eacd06677ace77da0c1d5
Valerio Gherardi <vgherard840@gmail.com>
2024-09-05 13:05:57 +0200

    Add 'plot_albufera_management2' sandbox helper

--- sandbox/plot_albufera_management.R
+++ sandbox/plot_albufera_management.R
@@ -14,3 +14,22 @@ plot_albufera_management <- function(management_df = albufera_management) {
                   y = "Height (cm)",
                   title = "Height vs Day of the Year")
 }
+
+plot_albufera_management2 <- function(management_df = albufera_management) {
+  management_df$day <- as.Date(paste("2000",
+                                      management_df$mm,
+                                      management_df$dd,
+                                      sep = "-"))
+  management_df$seed_day <-
+    as.numeric( management_df$day - as.Date("2000-04-20") )
+
+  management_df |>
+    ggplot2::ggplot(ggplot2::aes(x = seed_day, y = height_cm, color = tancat)) +
+    ggplot2::geom_line() +
+    ggplot2::facet_grid(variety ~ .) +
+    ggplot2::scale_x_continuous() +
+    ggplot2::scale_color_manual(values = c("TRUE" = "blue", "FALSE" = "red")) +
+    ggplot2::labs(x = "Seed Day",
+                  y = "Height (cm)",
+                  title = "Height vs Day of the Year")
+}
