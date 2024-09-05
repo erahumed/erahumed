5b9f9f85d2fd7e6b5364d23afe86c4c38410c298
Valerio Gherardi <vgherard840@gmail.com>
2024-09-05 11:16:36 +0200

    albufera_ca_schedules: Add 'application_type' column and rename 'variety' -> 'rice_variety' for clarity
    Former-commit-id: 168b7813283b117cfdb242442daaaa135d0faf67

--- data-raw/albufera_ca_schedules.R
+++ data-raw/albufera_ca_schedules.R
@@ -2,47 +2,43 @@ library(dplyr)
 
 # TODO: set amounts
 albufera_ca_schedules <- tribble(
-  ~day,   ~variety,       ~chemical,        ~amount,
-  51,     "J.Sendra",     "Acetamiprid",    0,
-  51,     "Bomba",        "Acetamiprid",    0,
-  51,     "Clearfield",   "Acetamiprid",    0,
+  ~day,   ~rice_variety,  ~chemical,        ~amount,        ~application_type,
+  51,     "J.Sendra",     "Acetamiprid",    0,              "ground",
+  51,     "Bomba",        "Acetamiprid",    0,              "ground",
+  51,     "Clearfield",   "Acetamiprid",    0,              "ground",
 
-  51,     "J.Sendra",     "Benta",          0,
-  51,     "Bomba",        "Benta",          0,
+  51,     "J.Sendra",     "Benta",          0,              "ground",
+  51,     "Bomba",        "Benta",          0,              "ground",
 
-  51,     "J.Sendra",     "MCPA",           0,
-  51,     "Bomba",        "MCPA",           0,
+  51,     "J.Sendra",     "MCPA",           0,              "ground",
+  51,     "Bomba",        "MCPA",           0,              "ground",
 
-  21,     "J.Sendra",     "Penoxulam",      0,
-  21,     "Bomba",        "Penoxulam",      0,
+  21,     "J.Sendra",     "Penoxulam",      0,              "ground",
+  21,     "Bomba",        "Penoxulam",      0,              "ground",
 
-  7,      "J.Sendra",     "Cyhalo",         0,
-  7,      "Bomba",        "Cyhalo",         0,
-  21,     "J.Sendra",     "Cyhalo",         0,
-  21,     "Bomba",        "Cyhalo",         0,
+  7,      "J.Sendra",     "Cyhalo",         0,              "ground",
+  7,      "Bomba",        "Cyhalo",         0,              "ground",
+  21,     "J.Sendra",     "Cyhalo",         0,              "ground",
+  21,     "Bomba",        "Cyhalo",         0,              "ground",
 
-  21,     "Clearfield",   "Cicloxidim",     0,
-  51,     "Clearfield",   "Cicloxidim",     0,
+  21,     "Clearfield",   "Cicloxidim",     0,              "ground",
+  51,     "Clearfield",   "Cicloxidim",     0,              "ground",
 
-  76,     "Bomba",        "Azoxy",          0,
-  90,     "Bomba",        "Azoxy",          0,
-  104,    "Bomba",        "Azoxy",          0,
-  76,     "J.Sendra",     "Azoxy",          0,
-  90,     "J.Sendra",     "Azoxy",          0,
-  NA,     "J.Sendra",     "Azoxy",          0,  # TODO: why?
-  76,     "Clearfield",   "Azoxy",          0,
-  90,     "Clearfield",   "Azoxy",          0,
-  NA,     "Clearfield",   "Azoxy",          0,  # TODO: why?
+  76,     "Bomba",        "Azoxy",          0,              "aerial",
+  90,     "Bomba",        "Azoxy",          0,              "aerial",
+  104,    "Bomba",        "Azoxy",          0,              "aerial",
+  76,     "J.Sendra",     "Azoxy",          0,              "aerial",
+  90,     "J.Sendra",     "Azoxy",          0,              "aerial",
+  76,     "Clearfield",   "Azoxy",          0,              "aerial",
+  90,     "Clearfield",   "Azoxy",          0,              "aerial",
 
-  76,     "Bomba",        "Difeno",         0,
-  90,     "Bomba",        "Difeno",         0,
-  104,    "Bomba",        "Difeno",         0,
-  76,     "J.Sendra",     "Difeno",         0,
-  90,     "J.Sendra",     "Difeno",         0,
-  NA,     "J.Sendra",     "Difeno",         0,  # TODO: why?
-  76,     "Clearfield",   "Difeno",         0,
-  90,     "Clearfield",   "Difeno",         0,
-  NA,     "Clearfield",   "Difeno",         0   # TODO: why?
+  76,     "Bomba",        "Difeno",         0,              "aerial",
+  90,     "Bomba",        "Difeno",         0,              "aerial",
+  104,    "Bomba",        "Difeno",         0,              "aerial",
+  76,     "J.Sendra",     "Difeno",         0,              "aerial",
+  90,     "J.Sendra",     "Difeno",         0,              "aerial",
+  76,     "Clearfield",   "Difeno",         0,              "aerial",
+  90,     "Clearfield",   "Difeno",         0,              "aerial"
 )
 
 usethis::use_data(albufera_ca_schedules, overwrite = TRUE)
