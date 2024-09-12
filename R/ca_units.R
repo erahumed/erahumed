2ec441674d488d2477d45ecdba68d38338e358f4
Valerio Gherardi <vgherard840@gmail.com>
2024-09-12 09:35:00 +0200

    ca: document in comment simpler would-be algorithm that does not work in practice

--- R/ca_units.R
+++ R/ca_units.R
@@ -58,5 +58,15 @@ ca_choose_application_day_index <- function(application_day,
   actual_day_idx <- which.min(abs(potential_days - application_day))
   actual_day <- potential_days[actual_day_idx]
 
-  which.max(seed_day_delayed == actual_day)
+  return( which.max(seed_day_delayed == actual_day) )
+
+  # The simpler approach below - which would allow to skip the computation of
+  # 'potential_days' is not correct, due to the fact that the 'application_day'
+  # in albufera_ca_schedules is slightly out of phase (by a few days) with the
+  # 'seed_day' in the albufera_management data.frame. Therefore, using this
+  # approach results in some ground application being made on days in which the
+  # cluster is not empty and/or not in a c(irrigation, draining) = c(F, F)
+  # status.
+  #
+  # which.max(seed_day_delayed == application_day)
 }
