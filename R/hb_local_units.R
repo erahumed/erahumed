8da5cf90948a2e75b5db12cc79442aac9d288f53
Valerio Gherardi <vgherard840@gmail.com>
2024-09-12 10:04:09 +0200

    Remove hb_local() duplicate definition
    Former-commit-id: 82a4f3c90bd8b2e063d80dbdff8649b7f483f9bf

--- R/hb_local_units.R
+++ R/hb_local_units.R
@@ -7,57 +7,6 @@ hbl_ditch_inflow_pct <- function(ditch, area)
   return(res)
 }
 
-
-hb_local <- function(
-    date,
-    ditch,
-    cluster_id,
-    ideal_height_cm,
-    petp_cm,
-    irrigation,
-    draining,
-    area_m2,
-    total_inflow_lake,
-    ideal_flow_rate_cm = 5,
-    ...
-    )
-{
-  pcts <- hbl_ditch_inflow_pct(ditch, area_m2)
-
-  capacity_m3_s <- total_inflow_lake * pcts$inflow_pct[match(ditch, pcts$ditch)]
-
-  res <- lapply(
-    unique(ditch),
-    function(dd) {
-      idx <- ditch == dd
-      args <- c(
-        list(ideal_height_cm = ideal_height_cm[idx],
-             petp_cm = petp_cm[idx],
-             irrigation = irrigation[idx],
-             draining = draining[idx],
-             area_m2 = area_m2[idx],
-             capacity_m3_s = capacity_m3_s[idx],
-             date = date[idx],
-             ideal_flow_rate_cm = ideal_flow_rate_cm,
-             cluster_id = cluster_id[idx],
-             ditch = ditch[idx]
-        ),
-        lapply(list(...), \(x) x[idx])
-      )
-
-      do.call(hbl_simulate_ditch, args)
-      }) |>
-    do.call(c, args = _) |>  # flatten to single list of data-frames
-    data.table::rbindlist() |>
-    as.data.frame()
-
-  # To substitute with a proper class constructor?
-  class(res) <- c("hb_local", "data.frame")
-  attr(class(res), "package") <- "erahumed"
-
-  return(res)
-}
-
 hbl_simulate_ditch <- function(
     ideal_height_cm,
     irrigation,
