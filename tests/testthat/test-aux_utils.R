680a3b88574abf85fcdcde17625107c4a51ecce6
Valerio Gherardi <vgherard840@gmail.com>
2024-09-12 08:29:36 +0200

    Format test

--- tests/testthat/test-aux_utils.R
+++ tests/testthat/test-aux_utils.R
@@ -5,7 +5,7 @@ test_that("moving_average() length is correct", {
   expect_length(moving_average(x, 5), length(x))
 })
 
-test_that("moving_average() returns the correct result in a basic case", {
+test_that("moving_average() gives the correct result in a basic case", {
   expect_equal(moving_average(1:3, 3), (1 / 3) * c(1 + 1 + 2,
                                                    1 + 2 + 3,
                                                    2 + 3 + 3)
@@ -16,7 +16,7 @@ test_that("moving_average() raises a warning when 'k' is even", {
   expect_warning(moving_average(1:5, 4))
 })
 
-test_that("moving_average() returns the correct result for even 'k'", {
+test_that("moving_average() gives the correct result for even 'k'", {
   res <- suppressWarnings(moving_average(1:5, 4))
   expect_equal(
     res,
@@ -30,7 +30,7 @@ test_that("moving_average() returns the correct result for even 'k'", {
 
 
 
-test_that("get_mm() and get_dd() returns vectors of same length of input", {
+test_that("get_mm() and get_dd() outputs have same length of input", {
   input <- as.POSIXlt(c("2021-01-01", "2019-12-31", "1992-01-24", "2001-10-02"))
   mm <- get_mm(input)
   dd <- get_dd(input)
@@ -39,7 +39,7 @@ test_that("get_mm() and get_dd() returns vectors of same length of input", {
   expect_length(dd, length(input))
   })
 
-test_that("get_mm() and get_dd() returns month and day numbers", {
+test_that("get_mm() and get_dd() return month and day numbers", {
   input <- as.POSIXlt(c("2021-01-01", "2019-12-31", "1992-01-24", "2001-10-02"))
   mm <- get_mm(input)
   dd <- get_dd(input)
@@ -48,7 +48,7 @@ test_that("get_mm() and get_dd() returns month and day numbers", {
   expect_in(dd, 1:31)
 })
 
-test_that("get_mm() and get_dd() returns the correct values in simple cases", {
+test_that("get_mm() and get_dd(): correct values in simple cases", {
   input <- as.POSIXlt(c("2021-01-01", "2019-12-31", "1992-01-24", "2001-10-02"))
   mm <- get_mm(input)
   dd <- get_dd(input)
@@ -56,6 +56,7 @@ test_that("get_mm() and get_dd() returns the correct values in simple cases", {
   expect_equal(mm, c(1, 12, 1, 10))
   expect_equal(dd, c(1, 31, 24, 2))
 })
+
 
 
 test_that("cm_day_to_m3_s() returns correct value in simple case", {
@@ -66,7 +67,7 @@ test_that("cm_day_to_m3_s() returns correct value in simple case", {
   expect_equal(cm_day_to_m3_s(input_cm, area_m2), expected)
 })
 
-test_that("unit conversion helpers return correct values in simple cases", {
+test_that("m3_s_to_cm_day() returns correct value in simple case", {
   input_m3_s <- 1  # 1 m3/s of water during 1 day
   area_m2 <- 1
 
