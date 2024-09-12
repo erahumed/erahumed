e5e29eb772ea8ede32a1e30acf083c764e67af0f
Valerio Gherardi <vgherard840@gmail.com>
2024-09-12 14:11:40 +0200

    Add outflow_extra column to variables for plotting hb_global object in Shiny

--- R/hb_helpers.R
+++ R/hb_helpers.R
@@ -6,6 +6,7 @@ hb_global_var_labs <- function(invert = FALSE) {
     outflow_pujol = "Pujol Outflow [m\u{00B3} / s]",
     outflow_perellonet = "Perellonet Outflow [m\u{00B3} / s]",
     outflow_perello = "Perello Outflow [m\u{00B3} / s]",
+    outflow_extra = "Extra Outflow [m\u{00B3} / s]",
     inflow_total = "Total Inflow [m\u{00B3} / s]",
     residence_time_days = "Residence Time [Days]"
   )
