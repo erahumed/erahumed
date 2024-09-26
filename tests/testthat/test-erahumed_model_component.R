2a4b9bf3af8310b2a685077993a01735efcb2538
Valerio Gherardi <vgherard840@gmail.com>
2024-09-26 11:47:40 +0200

    Basic tests for erahumed_model_component

--- tests/testthat/test-erahumed_model_component.R
+++ tests/testthat/test-erahumed_model_component.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Constructor succeeds with valid input", {
+  expect_no_error( new_model_component() )
+})
+
+test_that("Constructor throws an error if 'output' is not a data.frame", {
+  expect_error(
+    new_model_component(output = "Not a data.frame"),
+    class = "new_model_component_argcheck_error"
+    )
+})
+
+test_that("Constructor throws an error if 'params' is not a list", {
+  expect_error(
+    new_model_component(params = numeric(10)),
+    class = "new_model_component_argcheck_error"
+  )
 })
