4ed8ffe5a55928a490f1673a413ac915aa28b3d5
Valerio Gherardi <vgherard840@gmail.com>
2024-09-26 12:20:54 +0200

    Test that elements of erahumed_model list have the correct class

--- R/erahumed_model.R
+++ R/erahumed_model.R
@@ -9,6 +9,10 @@ is_erahumed_model <- function(obj) {
   if (!is.list(obj))
     return(FALSE)
 
+  elems_have_right_class <- sapply(obj, inherits, "erahumed_model_component")
+  if (!all(elems_have_right_class))
+    return(FALSE)
+
   if ( !inherits(obj, class(new_erahumed_model())) )
     return(FALSE)
 
--- tests/testthat/test-erahumed_model.R
+++ tests/testthat/test-erahumed_model.R
@@ -11,11 +11,17 @@ test_that("Validator returns TRUE on constructor output", {
   expect_true( is_erahumed_model(m) )
 })
 
-test_that("Validator returns FALSE if the underlying object is not a list", {
+test_that("Validator: FALSE if object is not a list", {
   obj <- structure("Not a list", class = class(erahumed_model()))
   expect_false( is_erahumed_model(obj) )
 })
 
-test_that("Validator returns FALSE on an ordinary list", {
+test_that("Validator: FALSE if list elements are not of the right S3 class", {
+  obj <- erahumed_model()
+  obj[["invalid_element"]] <- list()  # Not of 'model_component' class!
+  expect_false( is_erahumed_model(obj) )
+})
+
+test_that("Validator: FALSE if object is not of the right S3 class", {
   expect_false( is_erahumed_model(list()) )
 })
