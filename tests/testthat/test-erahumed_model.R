29ec4166af6aadf3c4620211b19f8b9f3e749a91
Valerio Gherardi <vgherard840@gmail.com>
2024-09-26 11:36:37 +0200

    Stricter tests for is_erahumed_model()

--- tests/testthat/test-erahumed_model.R
+++ tests/testthat/test-erahumed_model.R
@@ -10,3 +10,12 @@ test_that("Validator returns TRUE on constructor output", {
   m <- erahumed_model()
   expect_true( is_erahumed_model(m) )
 })
+
+test_that("Validator returns FALSE if the underlying object is not a list", {
+  obj <- structure("Not a list", class = class(erahumed_model()))
+  expect_false( is_erahumed_model(obj) )
+})
+
+test_that("Validator returns FALSE on an ordinary list", {
+  expect_false( is_erahumed_model(list()) )
+})
