aea127c42415148ee412712ccbb52240f07c566e
Valerio Gherardi <vgherard840@gmail.com>
2024-10-08 13:11:43 +0200

    CT reimplementation draft

--- R/ct--low-level.R
+++ R/ct--low-level.R
@@ -9,9 +9,11 @@ ct_to_cluster_wrap <- function(cluster_ca_df)
                  ct_to_cluster(
                    application_kg = cluster_ca_df[[chemical]],
                    rain_mm = cluster_ca_df[["rain_mm"]],
+                   etp_mm = cluster_ca_df[["evapotranspiration_mm"]],
                    temperature = rnorm(nrow(cluster_ca_df)),
                    height_cm = cluster_ca_df[["real_height_cm"]],
                    outflow_m3_s = cluster_ca_df[["real_outflow_m3_s"]],
+                   inflow_m3_s = cluster_ca_df[["real_inflow_m3_s"]],
                    area_m2 = cluster_ca_df[["area_m2"]][[1]],
                    seed_day = cluster_ca_df[["seed_day"]],
                    chemical = chemical
@@ -23,53 +25,64 @@ ct_to_cluster_wrap <- function(cluster_ca_df)
 
 ct_to_cluster <- function(application_kg,
                           rain_mm,
+                          etp_mm,
                           temperature,
                           height_cm,
                           outflow_m3_s,
+                          inflow_m3_s,
                           area_m2,
                           seed_day,
                           chemical)
 {
-  height_m <- height_cm / 100
-  volume_m3 <- height_m * area_m2
-  outflow_m3 <- outflow_m3_s * s_per_day()
-  rain_cm <- rain_mm / 10
-
-  ode_model <- get_ode_model(rain_cm = rain_cm,
+  ode_model <- get_ode_model(area_m2 = area_m2,
+                             rain_mm = rain_mm,
+                             etp_mm = etp_mm,
                              temperature = temperature,
-                             height_m = height_m,
+                             height_cm = height_cm,
                              volume_m3 = volume_m3,
-                             outflow_m3 = outflow_m3,
+                             outflow_m3_s = outflow_m3_s,
+                             inflow_m3_s = inflow_m3_s,
                              application_kg = application_kg,
                              seed_day = seed_day)
 
   initial_state <- c(mf = 0, mw = 0, ms = 0)
 
-  n_time_steps <- length(application_kg) - 1
+  n_time_steps <- length(application_kg)
   res <- ode_solve(func = ode_model, n_time_steps = n_time_steps)
 
   names(res) <- paste0(chemical, " (", c("F", "W", "S"), ")")
   return(res)
 }
 
-get_ode_model <- function(rain_cm,
+get_ode_model <- function(area_m2,
+                          rain_mm,
+                          etp_mm,
                           temperature,
-                          height_m,
+                          height_cm,
                           volume_m3,
-                          outflow_m3,
+                          outflow_m3_s,
+                          inflow_m3_s,
                           application_kg,
                           seed_day
-                          ) {
+                          )
+{
+  height_m <- height_cm / 100
+  volume_m3 <- height_m * area_m2
+  outflow_m3 <- outflow_m3_s * s_per_day()
+  inflow_m3 <- inflow_m3_s * s_per_day()
+  rain_cm <- rain_mm / 10
+  rain_m3 <- (rain_cm / 100) * area_m2
+  etp_m3 <- (etp_mm / 1000) * area_m2
 
-  drift <- 0  # I     # Fracción perdida por deriva
-  covmax <- 0.5  # I   # Cobertura máxima del cultivo
-  jgrow <- 152    #I    Días totales entre emergencia y maduración
-  sa <- 114881.779  # C    # Superficie del arrozal en m2
-  kf <- 0.24     # P      Tasa de degradación en el follaje (día-1)
-  kw <- 0.046     # P    Tasa de degradación en el agua (día-1)
-  Q10_kw <- 2.6    # P
-  temp_kw <- 20    # P
-  ks_sat <- 0.03   # P            Tasa de degradación en el sedimento (día-1)
+  drift <- 0        # I    Fracción perdida por deriva
+  covmax <- 0.5     # I    Cobertura máxima del cultivo
+  jgrow <- 152      # I    Días totales entre emergencia y maduración
+  sa <- 114881.779  # C    Superficie del arrozal en m2
+  kf <- 0.24        # P    Tasa de degradación en el follaje (día-1)
+  kw <- 0.046       # P    Tasa de degradación en el agua (día-1)
+  Q10_kw <- 2.6     # P
+  temp_kw <- 20     # P
+  ks_sat <- 0.03    # P    Tasa de degradación en el sedimento (día-1)
   Q10_ks_sat <- 2.6 # P
   temp_ks_sat <- 20 # P
   ks_unsat <- 0.117 # P
@@ -101,7 +114,6 @@ get_ode_model <- function(rain_cm,
   kdifus <- 69.35 / 365 - pos * ((MW)^(-2/3))  # metros / dia
   fpw <- (kd * css) / (1 + kd * css)  # Porcentaje pesticida particulado en agua
 
-
   # Precomputed time series
   igrow <- seed_day |> pmax2(0) |> pmin2(jgrow)
   cover <- covmax * (igrow / jgrow)
@@ -114,64 +126,59 @@ get_ode_model <- function(rain_cm,
   mwapp <- m_app_kg * (1 - cover) * (1 - SNK) * (!is_empty)
   msapp <- m_app_kg * (1 - cover) * (1 - SNK) * (dinc / dact) * is_empty
 
-  # Volume for computing density.
-  # TODO: Critical, how should we define this density when the water level is
-  # zero? ATM, this is defined taking the daily outflow as the relevant volume,
-  # but we should think whether this is exactly what we want... and this should
-  # likely be determined by how this density enters the equations below.
-  cw_multiplier <-
-    (1-is_empty) / pmax2(volume_m3, 1e-10) +
-    is_empty * (outflow_m3 > 0) / pmax2(outflow_m3, 1e-10)
+  vol_t <- c(NA, volume_m3)
+  vol_t_petp <- vol_t + c(NA, rain_m3)
+  vol_tp1 <- vol_t_petp + c(NA, inflow_m3 - outflow_m3)
 
-  msetl_multiplier <- -(1-is_empty) * ksetl * (fpw / pmax2(height_m, 1e-10))
-
-  mdifus_mult_ms <- (1-is_empty) * kdifus * sa * fds  / pos / vsed
-  mdifus_mult_cw <- -(1-is_empty) * kdifus * sa * fdw
-
-  # Applying Arrhenius kinetic equilibrium
-
+  # Degradation (applying Arrhenius kinetic equilibrium)
   kw <- kw * (Q10_kw ^ ((temperature - temp_kw) / 10))
   ks_sat <- ks_sat * (Q10_ks_sat ^ ((temperature - temp_ks_sat) / 10))
   ks_unsat <- ks_unsat * (Q10_ks_unsat ^ ((temperature - temp_ks_unsat) / 10))
-
   ks <- (1-is_empty) * ks_sat + is_empty * ks_unsat
 
-  mwdeg_multiplier <- -(1 - exp(-kw * dt))
-  msdeg_multiplier <- -(1 - exp(-ks * dt))
-  mfdeg_multiplier <- -(1 - exp(-kf * dt))
+  deg_fac_mf <- replicate(length(application_kg), exp(-kf * dt))
+  deg_fac_mw <- exp(-kw * dt)
+  deg_fac_ms <- exp(-ks * dt)
 
-  mwash_multiplier <- -(1 - exp(-fet * rain_cm * dt))
-
-  Aff <- mfdeg_multiplier + mwash_multiplier
-  Awf <- -mwash_multiplier
-  Aww <- mwdeg_multiplier + msetl_multiplier
-  Aws <- mdifus_mult_ms
-  Asw <- -msetl_multiplier
-  Ass <- msdeg_multiplier - mdifus_mult_ms
-  bw <- mdifus_mult_cw - outflow_m3
-  bs <- -mdifus_mult_cw
+  washout_fac <- 1 - exp(-fet * rain_cm * dt)
 
   f <- function(t, mf, mw, ms)
   {
 
-    cw_max <- cw_multiplier[[t]] * mw
-    cw <- if (cw_max < sol) cw_max else sol  # The non-linearity
-    sol_diff <- (cw_max - cw) * volume_m3[[t]]
+    # Settling
+    # TODO: deal with height_m == 0
+    msetl <- ksetl * fpw * mw / height_m[[t]]
+    mw <- mw - msetl
+    ms <- ms + msetl
 
-    mw <- (mw - sol_diff) * (1 - is_empty[[t]])
-    ms <- (ms + sol_diff)
+    # Diffusion
+    # TODO: deal with vol_t == 0
+    mdifus <- kdifus * sa * ((fds / pos) * ms / vsed - fdw * mw / vol_t[[t]])
+    mw <- mw + mdifus
+    ms <- ms - mdifus
 
-    delta_mf <-
-      mfapp[[t]] + Aff[[t]]*mf
+    # Degradation
+    mf <- deg_fac_mf[[t]] * mf
+    mw <- deg_fac_mw[[t]] * mw
+    ms <- deg_fac_ms[[t]] * ms
 
-    delta_mw <-
-      mwapp[[t]] + Awf[[t]]*mf + Aww[[t]]*mw + Aws[[t]]*ms + bw[[t]]*cw
+    # Washout
+    mwashout <- washout_fac[[t]] * mf
+    mf <- mf - mwashout
+    mw <- mw + mwashout
 
-    delta_ms <-
-      msapp[[t]]               + Asw[[t]]*mw + Ass[[t]]*ms - bs[[t]]*cw
+    # Outflow
+    mw <- (1 - outflow_m3[[t]] / vol_t_petp[[t]]) * mw
 
-    return(c(delta_mf, delta_mw, delta_ms))
+    # Inflow (not implemented ATM!)
+    mw <- mw + inflow_m3[[t]] * 0
 
+    # Application
+    mf <- mf + mfapp[[t]]
+    mw <- mw + mwapp[[t]]
+    ms <- ms + msapp[[t]]
+
+    return(c(mf, mw, ms))
   }
 
   f
@@ -180,16 +187,16 @@ get_ode_model <- function(rain_cm,
 
 euler_base <- function(func, n_time_steps = n_time_steps) {
   # Preallocate space for solution
-  mf <- numeric(n_time_steps + 1)
-  mw <- numeric(n_time_steps + 1)
-  ms <- numeric(n_time_steps + 1)
+  mf <- numeric(n_time_steps)
+  mw <- numeric(n_time_steps)
+  ms <- numeric(n_time_steps)
 
   # Euler's method loop
-  for (t in 1:n_time_steps) {
-    deltas <- func(t = t, mf = mf[[t]], mw = mw[[t]], ms = ms[[t]])
-    mf[[t + 1]] <- mf[[t]] + deltas[[1]]
-    mw[[t + 1]] <- mw[[t]] + deltas[[2]]
-    ms[[t + 1]] <- ms[[t]] + deltas[[3]]
+  for (t in 2:n_time_steps) {
+    new_m <- func(t = t, mf = mf[[t-1]], mw = mw[[t-1]], ms = ms[[t-1]])
+    mf[[t]] <- new_m[[1]]
+    mw[[t]] <- new_m[[2]]
+    ms[[t]] <- new_m[[3]]
   }
 
   # Return the results as a data frame of states
