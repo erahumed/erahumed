3c4018f61edea5efa34925c603d8ee66cf780271
Valerio Gherardi <vgherard840@gmail.com>
2024-10-17 16:01:35 +0200

    CA: minimal Shiny interface (#129)
    * Ultra-simplistic draft of Shiny component
    
    * Update NEWS

--- R/ca--shiny.R
+++ R/ca--shiny.R
@@ -1,9 +1,39 @@
 caUI <- function(id) {
   ns <- shiny::NS(id)
 
+  clusters <- erahumed::albufera_clusters$cluster_id
+
   shiny::tabsetPanel(
-    shiny::tabPanel("Output", NULL),
-    shiny::tabPanel("Setup", NULL)
+    shiny::tabPanel(
+      "Output",
+
+      shiny::fluidRow(
+        shiny::column(4,
+                      shiny::selectInput(ns("cluster_id"),
+                                         "Select Cluster",
+                                         choices = clusters,
+                                         selected = clusters[[1]]
+                      )
+        )
+      ),
+
+      shiny::fluidRow(
+        shiny::column(4, leaflet::leafletOutput(ns("albufera_map"))),
+        shiny::column(8, plotly::plotlyOutput(ns("plot")))
+        )
+      ),
+
+
+    shiny::tabPanel("Application Schedules", csvInputUI(ns("ca_schedules"))),
+
+    shiny::tabPanel("Setup",
+                    shiny::numericInput(ns("seed"),
+                                        "Seed for simulation",
+                                        value = 840,
+                                        step = 1),
+                    )
+
+
   )
 
 
@@ -11,6 +41,40 @@ caUI <- function(id) {
 
 caServer <- function(id, model) {
   shiny::moduleServer(id, function(input, output, session) {
-    NULL
+    ns <- session$ns
+
+    ca_schedules_df <- csvInputServer("ca_schedules",
+                                    erahumed::albufera_ca_schedules)
+
+    res <- shiny::reactive({
+      compute_ca(model = model(), ca_schedules_df = ca_schedules_df())
+      })
+
+    output$albufera_map <- leaflet::renderLeaflet(
+      tryCatch(
+        plot_albufera_clusters(seed = input$seed),
+        error = function(cnd) {
+          cat("Error while loading Albufera Leaflet map.")
+          return(NULL)
+        },
+        warning = function(cnd) {
+          return(NULL)
+        }
+      )
+    )
+
+    shiny::observeEvent(input$albufera_map_shape_click, {
+      click <- input$albufera_map_shape_click
+      if (!is.null(click)) {
+        shiny::updateSelectInput(session, "cluster_id", selected = click$id)
+      }
+    })
+
+    output$plot <- plotly::renderPlotly({
+      shiny::req(input$cluster_id)
+      plot(ca(res()), cluster_id = input$cluster_id)
+    })
+
+    return(res)
   })
 }
