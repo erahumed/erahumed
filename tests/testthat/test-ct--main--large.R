6a73595fa4190db5b082327c609a559d80553f53
Valerio Gherardi <vgherard840@gmail.com>
2024-10-17 12:21:23 +0200

    CT: test positivity of mass time series

--- tests/testthat/test-ct--main--large.R
+++ tests/testthat/test-ct--main--large.R
@@ -9,6 +9,19 @@ test_that("Returned dataset has the expected number of rows", {
   expect_equal(nrow(test_df), n_clusters * n_days)
 })
 
+test_that("The time series of chemical masses are always positive", {
+  lower_thresh <- -1e-9  # -1 microgram looks fair enough
+  test_df <- component_output(test_mod_large(), "ct")
+
+  only_positive_values <- test_df |>
+    dplyr::select(-c(cluster_id, date)) |>  # Select relevant columns
+    (\(df) df < lower_thresh)() |>          # Applied threshold column-wise
+    rowSums() |>  # Vec of number of cols that satisfy condition for each row
+    (\(x) all(x == 0))()
+
+  expect_true(only_positive_values)
+})
+
 test_that("simple snapshot is constant", {
   skip_on_ci()  # Gives inconsistent result across different platforms
 
