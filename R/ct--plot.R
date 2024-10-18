bdfb60f8d327fa2297f09445fe8d082525a49374
Valerio Gherardi <vgherard840@gmail.com>
2024-10-18 10:26:46 +0200

    ct--plot skeleton allowing to choose between cluster and timeline view

--- R/ct--plot.R
+++ R/ct--plot.R
@@ -12,7 +12,23 @@
 #' @return A plotly plot.
 #'
 #' @export
-plot.erahumed_ct <- function(x, ...) {
-  cat("Not yet implemented.")
+plot.erahumed_ct <- function(x, type = c("cluster_view", "timeline_view"), ...)
+{
+  type <- match.arg(type)
+  switch(type,
+         cluster_view = plot_erahumed_ct_cluster_view(x, ...),
+         timeline_view = plot_erahumed_ct_timeline_view(x, ...)
+  )
+
   return(invisible(NULL))
+}
+
+plot_erahumed_ct_cluster_view <- function(x, ...) {
+  p <- NULL
+
+  return(p)
+}
+
+plot_erahumed_ct_timeline_view <- function(x, ...) {
+  stop("Not yet implemented.")
 }
