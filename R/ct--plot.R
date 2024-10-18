8043ca73015c201d04de454da36965b2bd1a1015
Valerio Gherardi <vgherard840@gmail.com>
2024-10-18 11:06:13 +0200

    Basic plot of masses over time

--- R/ct--plot.R
+++ R/ct--plot.R
@@ -15,16 +15,27 @@
 plot.erahumed_ct <- function(x, type = c("cluster_view", "timeline_view"), ...)
 {
   type <- match.arg(type)
+
   switch(type,
          cluster_view = plot_erahumed_ct_cluster_view(x, ...),
          timeline_view = plot_erahumed_ct_timeline_view(x, ...)
-  )
-
-  return(invisible(NULL))
+         )
 }
 
 plot_erahumed_ct_cluster_view <- function(x, ...) {
-  p <- NULL
+  args <- list(...)
+
+  if ( !("cluster_id" %in% names(args)) )
+    stop("Please specify cluster to plot through the 'cluster_id' argument.")
+
+  df <- component_output(x)
+  df <- df[df$cluster_id == args$cluster_id, ]
+
+  p <- plotly::plot_ly(data = df, x = ~date)
+
+  chem_names <- names(df)[ -c(1:2) ]  # Exclude 'date' and 'cluster_id' columns
+  for (col in chem_names)
+    p <- p |> plotly::add_lines(y = df[[col]], name = col)
 
   return(p)
 }
