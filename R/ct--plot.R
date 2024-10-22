d2b11285381307d47371671be620f18bc497fea7
Valerio Gherardi <vgherard840@gmail.com>
2024-10-22 12:50:25 +0200

    CT: add "density" plot

--- R/ct--plot.R
+++ R/ct--plot.R
@@ -6,6 +6,7 @@
 #' @param x An object of class `ct`.
 #' @param type Type of plot to be generated. Currently, only the
 #' `"cluster_view"` plot type is implemented.
+#' @param variable Variable to plot. Either "mass" or "density".
 #' @param ... Further plotting parameters, associated with the plot type
 #' specified by `type`; See details.
 #' @param ... Further plotting parameters.
@@ -20,17 +21,25 @@
 #' @return A plotly plot.
 #'
 #' @export
-plot.erahumed_ct <- function(x, type = c("cluster_view", "timeline_view"), ...)
+plot.erahumed_ct <- function(
+    x,
+    type = c("cluster_view"),
+    variable = c("mass", "density"),
+    ...
+    )
 {
   type <- match.arg(type)
+  variable <- match.arg(variable)
 
-  switch(type,
-         cluster_view = plot_erahumed_ct_cluster_view(x, ...),
-         timeline_view = plot_erahumed_ct_timeline_view(x, ...)
+  fun <- paste(type, variable, sep = "_")
+
+  switch(fun,
+         cluster_view_mass = plot_ct_cluster_view_mass(x, variable, ...),
+         cluster_view_density = plot_ct_cluster_view_density(x, variable, ...)
          )
 }
 
-plot_erahumed_ct_cluster_view <- function(x, ...) {
+plot_ct_cluster_view_mass <- function(x, ...) {
   args <- list(...)
 
   if ( !("cluster_id" %in% names(args)) )
@@ -78,6 +87,50 @@ plot_erahumed_ct_cluster_view <- function(x, ...) {
   return(p)
 }
 
-plot_erahumed_ct_timeline_view <- function(x, ...) {
-  stop("Not yet implemented.")
+plot_ct_cluster_view_density <- function(x, ...) {
+  args <- list(...)
+
+  if ( !("cluster_id" %in% names(args)) )
+    stop("Please specify cluster to plot through the 'cluster_id' argument.")
+
+  df <- component_output(x)
+  df <- df[df$cluster_id == args$cluster_id, ]
+
+  p <- plotly::plot_ly()
+
+  chemicals <- unique(df$chemical)  # Exclude 'date' and 'cluster_id' columns
+  for (chemical in chemicals) {
+    chem_df <- df[df$chemical == chemical, ]
+    p <- p |>
+      plotly::add_lines(
+        data = chem_df,
+        x = ~date,
+        y = ~cw_outflow,
+        line = list(width = 1.5, color = "#0099FF"),
+        name = paste(chemical, "(F)")
+      ) |>
+      plotly::add_lines(
+        data = chem_df,
+        x = ~date,
+        y = ~cw,
+        line = list(width = 1.5, color = "darkblue"),
+        name = paste(chemical, "(W)")
+      ) |>
+      plotly::add_lines(
+        data = chem_df,
+        x = ~date,
+        y = ~cs,
+        line = list(width = 1.5, color = "#AA5500"),
+        name = paste(chemical, "(S)")
+      )
+  }
+
+
+  p <- p |>
+    plotly::layout(title = paste("Time Series of chemical masses [Kg]"),
+                   xaxis = list(title = "Date"),
+                   yaxis = list(title = "Mass [kg]")
+    )
+
+  return(p)
 }
--- tests/testthat/test-ct--plot.R
+++ tests/testthat/test-ct--plot.R
@@ -1,7 +1,13 @@
-test_that("plot.erahumed_ct does not produce an error with valid inputs", {
+test_that("No error with basic valid inputs", {
   ct_obj <- ct(test_mod_small())
   cluster_id <- component_output(ct_obj)$cluster_id[[1]]
 
   expect_no_error( plot(ct_obj, cluster_id = cluster_id) )
 })
 
+test_that("No error with variable = 'density'", {
+  ct_obj <- ct(test_mod_small())
+  cluster_id <- component_output(ct_obj)$cluster_id[[1]]
+
+  expect_no_error( plot(ct_obj, variable = "density", cluster_id = cluster_id) )
+})
