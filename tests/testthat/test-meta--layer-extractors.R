4ea8f8a5c1d58f661326b03b391b573b07d69cfa
Valerio Gherardi <vgherard840@gmail.com>
2024-11-11 09:32:38 +0100

    Remove duplicate test file

--- tests/testthat/test-meta--layer-extractors.R
+++ tests/testthat/test-meta--layer-extractors.R
@@ -7,3 +7,8 @@ test_that("get_layer() throws an error if no layer is specified", {
   sim <- test_sim_small()
   expect_error(get_layer(sim))
 })
+
+test_that("get_layer() throws an error for invalid 'layer' argument", {
+  simulation <- test_sim_small()
+  expect_error( get_layer(simulation, "unexisting_layer") )
+})
