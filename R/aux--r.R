d6e8701e8e20d09337d5f9543e9c783edc000493
Valerio Gherardi <vgherard840@gmail.com>
2024-11-12 12:22:41 +0100

    Remove unused 'capture_params' helper

--- R/aux--r.R
+++ R/aux--r.R
@@ -2,18 +2,3 @@ succeeds <- function(expr) {
   result <- try(expr, silent = TRUE)
   !inherits(result, "try-error")
 }
-
-
-
-capture_params <- function(
-    fun = sys.function(sys.parent()),
-    envir = parent.frame()
-)
-{
-  forms <- formals(fun = fun, envir = envir)
-  arg_names <- names(forms)[names(forms) != "simulation"]
-  args_list <- lapply(arg_names, get, envir = envir)
-  names(args_list) <- arg_names
-
-  return(args_list)
-}
