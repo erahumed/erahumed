666516c0b4cbc02b568dbcebf272fd1edff80181
Valerio Gherardi <vgherard840@gmail.com>
2024-11-25 14:20:10 +0100

    generate_clusters_variety(): handle cases in which allocated proportion for Bomba and Clearfield are too high (close #202)

--- R/aux--clusters.R
+++ R/aux--clusters.R
@@ -46,6 +46,15 @@ generate_clusters_variety <- function(variety_prop) {
   area_bomba_target <- variety_prop[[2]] * area_tot
   area_clearfield_target <- variety_prop[[3]] * area_tot
 
+  area_bomba_max <- sum(res$area[res$tancat])
+  if (area_bomba_target > area_bomba_max) {
+    msg <- "Surface proportion allocated to 'Bomba' was too high."
+
+    msg <- paste0(msg, " Reduced to ", area_bomba_max / area_tot)
+    warning(msg)
+    area_bomba_target <- 0.999 * area_bomba_max  # 0.999 to ensure while() ends.
+  }
+
   ditches_clearfield <- paste0("d", 1:19)
 
   area_bomba <- 0
@@ -56,6 +65,17 @@ generate_clusters_variety <- function(variety_prop) {
       next
     res$variety[i] <- "Bomba"
     area_bomba <- area_bomba + res$area[i]
+  }
+
+  area_clearfield_max <- res$area[
+    res$ditch %in% ditches_clearfield & is.na(res$variety)
+    ] |> sum()
+  if (area_clearfield_target > area_clearfield_max) {
+    msg <- "Surface proportion allocated to 'Clearfield' was too high."
+
+    msg <- paste0(msg, " Reduced to ", area_clearfield_max / area_tot)
+    warning(msg)
+    area_clearfield_target <- 0.999 * area_clearfield_max  # 0.999 to ensure while() ends.
   }
 
   area_clearfield <- 0
--- tests/testthat/test-aux--clusters.R
+++ tests/testthat/test-aux--clusters.R
@@ -10,5 +10,14 @@ test_that("generate_clusters_variety(c(0.8, 0.1, 0.1)) succeeds", {
   expect_no_error( generate_clusters_variety(c(0.8, 0.1, 0.1)) )
 })
 
+test_that("generate_clusters_variety(c(1, 100, 1)) succeeds with a warning", {
+  expect_warning( generate_clusters_variety(c(1, 100, 1)) )
+})
+
+test_that("generate_clusters_variety(c(1, 1, 100)) succeeds with a warning", {
+  expect_warning( generate_clusters_variety(c(1, 1, 100)) )
+})
+
+
 
 
