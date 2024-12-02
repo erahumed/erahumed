3b129fc4c861a32a1f41dabe53f08ebc17c8e095
Valerio Gherardi <vgherard840@gmail.com>
2024-12-02 13:07:47 +0100

    (aside: update plot_albufera_management.R)

--- sandbox/plot_albufera_management.R
+++ sandbox/plot_albufera_management.R
@@ -4,15 +4,21 @@ plot_albufera_management <- function(management_df = albufera_management) {
                                       management_df$dd,
                                       sep = "-"))
 
-  management_df |>
-    ggplot2::ggplot(ggplot2::aes(x = date, y = height_cm, color = tancat)) +
-    ggplot2::geom_line() +
+  data_tancat <- management_df |> dplyr::filter(tancat)
+  data_no_tancat <- management_df |> dplyr::filter(!tancat)
+
+  ggplot2::ggplot(data = NULL,
+                  ggplot2::aes(x = date, y = ideal_height_eod_cm, color = tancat)
+                  ) +
+    ggplot2::geom_line(data = data_tancat, linetype = "solid", color = "black") +
+    ggplot2::geom_line(data = data_no_tancat, linetype = "dashed", color = "black") +
     ggplot2::facet_grid(variety ~ .) +
     ggplot2::scale_x_date(date_labels = "%m/%d", date_breaks = "1 month") +
-    ggplot2::scale_color_manual(values = c("TRUE" = "blue", "FALSE" = "red")) +
-    ggplot2::labs(x = "Day of the Year",
-                  y = "Height (cm)",
-                  title = "Height vs Day of the Year")
+    ggplot2::labs(x = "Día", y = "Profundidad [cm]") +
+    ggplot2::ggtitle(
+      label = "Profundidad teórica VS. día del año",
+      subtitle = "Profundidades teóricas en los arrozales del P.N. de l´Albufera durante el cultivo del arroz"
+    )
 }
 
 plot_albufera_management2 <- function(management_df = albufera_management) {
@@ -24,7 +30,9 @@ plot_albufera_management2 <- function(management_df = albufera_management) {
     as.numeric( management_df$day - as.Date("2000-04-20") )
 
   management_df |>
-    ggplot2::ggplot(ggplot2::aes(x = seed_day, y = height_cm, color = tancat)) +
+    ggplot2::ggplot(ggplot2::aes(x = seed_day,
+                                 y = ideal_height_eod_cm,
+                                 color = tancat)) +
     ggplot2::geom_line() +
     ggplot2::facet_grid(variety ~ .) +
     ggplot2::scale_x_continuous() +
