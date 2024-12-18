5758e2901219a10eb4c0a22690caaa4b54f3efd3
Valerio Gherardi <vgherard840@gmail.com>
2024-12-18 13:14:31 +0100

    Increase app load timeout in shinytest2

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -4,7 +4,7 @@ test_that("Initial Shiny values are consistent", {
   skip_on_ci()
 
   app <- dss()
-  drv <- AppDriver$new(app, name = "ERAHUMED")
+  drv <- AppDriver$new(app, name = "ERAHUMED", load_timeout = 60000)
 
   drv$expect_values()
 })
