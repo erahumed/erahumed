50c2662d85e9598ca46bc530b4ef4d9d71b07791
Valerio Gherardi <vgherard840@gmail.com>
2024-12-19 11:12:56 +0100

    Last minute changes to DSS output

--- R/dss--output.R
+++ R/dss--output.R
@@ -3,15 +3,20 @@ dss_output_ui <- function(id) {
   ns <- shiny::NS(id)
   bslib::page_fillable(
     title = "Input",
+    shiny::selectInput("selected_cluster_id",
+                       label = "Select cluster",
+                       choices = info_clusters()$cluster_id,
+                       selected = info_clusters()$cluster_id[[1]]
+                       ),
     bslib::layout_column_wrap(
-      card(card_header("Hidrology"), card_body(
+      card(card_header("Hidrology"), full_screen = TRUE,
         dygraphs::dygraphOutput(ns("hba_plot")),
-        dygraphs::dygraphOutput(ns("ca_plot"))
-        )),
-      card(card_header("Chemicals"), card_body(
+        dygraphs::dygraphOutput(ns("ca_plot")),
+        ),
+      card(card_header("Chemicals"), full_screen = TRUE,
         dygraphs::dygraphOutput(ns("ct_plot"))
-        )),
-      card(card_header("Effects"), card_body( "Not yet implemented." ))
+        ),
+      card(card_header("Effects"), full_screen = TRUE, "Not yet implemented.")
     )
   )
 }
@@ -19,8 +24,12 @@ dss_output_ui <- function(id) {
 dss_output_server <- function(id, layers) {
   shiny::moduleServer(id, function(input, output, session) {
     output$hba_plot <- dygraphs::renderDygraph(plot(layers$hba))
-    output$ca_plot <- dygraphs::renderDygraph(plot(layers$ca))
-    output$ct_plot <- dygraphs::renderDygraph(plot(layers$ct))
+    output$ca_plot <- dygraphs::renderDygraph(
+      plot(layers$ca, cluster_id = input$selected_cluster_id)
+      )
+    output$ct_plot <- dygraphs::renderDygraph(
+      plot(layers$ct, cluster_id = input$selected_cluster_id)
+      )
   })
 }
 
