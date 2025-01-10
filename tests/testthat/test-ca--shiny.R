a5b2856ac78e6f2cd50be10474cde96edc12ff15
Valerio Gherardi <vgherard840@gmail.com>
2025-01-10 12:25:54 +0100

    Remove dss-v0

--- tests/testthat/test-ca--shiny.R
+++ tests/testthat/test-ca--shiny.R
@@ -1,24 +1,10 @@
 test_that("UI succeeds", {
-  expect_no_error(caUI("ui"))
+  expect_no_error(ca_input_ui("ui"))
 })
 
-s <- test_sim_small()
-args <- list(
-  inp = shiny::reactiveVal(get_layer(s, "inp")),
-  hba = shiny::reactiveVal(get_layer(s, "hba")),
-  hbp = shiny::reactiveVal(get_layer(s, "hbp")),
-  shared = shiny::reactiveValues(
-    selected_cluster_id = albufera_clusters$cluster_id[1]
-  )
-)
-
-shiny::testServer(caServer, args = args, {
-  session$setInputs(seed = 840, cluster_id = albufera_clusters$cluster_id[1])
+shiny::testServer(ca_input_server, {
 
   expect_no_error(session$returned())
-  expect_s3_class(session$returned(), "erahumed_ca")
+  expect_vector(session$returned(), list())
 
-  expect_no_error(output$plot)
-
-  expect_no_error(output$downloadData)
 }) |> suppressMessages()
