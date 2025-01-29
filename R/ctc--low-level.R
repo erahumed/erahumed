84d697978603229811e3001fc2b43401f086c8f1
Valerio Gherardi <vgherard840@gmail.com>
2025-01-29 09:01:06 +0100

    Refactor CTC low-level helpers

--- R/ctc--low-level.R
+++ R/ctc--low-level.R
@@ -1,57 +1,80 @@
-ctc_to_cluster_wrap <- function(cluster_ca_df,
-                               drift,
-                               covmax,
-                               jgrow,
-                               SNK,
-                               dact_m,
-                               css_ppm,
-                               foc,
-                               bd_g_cm3,
-                               qseep_m_day,
-                               wilting,
-                               fc
-)
-{
-  res_template <- list(
-    cluster_id = cluster_ca_df[["cluster_id"]],
-    date = cluster_ca_df[["date"]]
-  )
+.compute_ctc <- function(simulation) {
+  .par <- get_layer_parameters(simulation, "ctc")
 
-  compute_masses <- function(chemical) {
-    ct_time_series(
-      application_kg = cluster_ca_df[[chemical]],
-      precipitation_mm = cluster_ca_df[["precipitation_mm"]],
-      etp_mm = cluster_ca_df[["evapotranspiration_mm"]],
-      temperature_ave = cluster_ca_df[["temperature_ave"]],
-      temperature_min = cluster_ca_df[["temperature_min"]],
-      temperature_max = cluster_ca_df[["temperature_max"]],
-      height_eod_cm = cluster_ca_df[["height_eod_cm"]],
-      outflow_m3_s = cluster_ca_df[["outflow_m3_s"]],
-      inflows_m3_s = list(cluster_ca_df[["inflow_m3_s"]]),
-      inflows_densities_kg_m3 = list(0),
-      area_m2 = cluster_ca_df[["area_m2"]][[1]],
-      seed_day = cluster_ca_df[["seed_day"]],
-      chemical = chemical,
-      drift = drift,
-      covmax = covmax,
-      jgrow = jgrow,
-      SNK = SNK,
-      dact_m = dact_m,
-      css_ppm = css_ppm,
-      foc = foc,
-      bd_g_cm3 = bd_g_cm3,
-      qseep_m_day = qseep_m_day,
-      wilting = wilting,
-      fc = fc
-    )
-  }
+  get_layer_output(simulation, "ca") |>
+    data.table::as.data.table() |>
+    merge(get_layer_output(simulation, "inp"), by = "date", sort = TRUE) |>
+    collapse::rsplit(by = ~ cluster_id,
+                     flatten = TRUE,
+                     use.names = FALSE,
+                     simplify = FALSE,
+                     keep.by = TRUE
+                     ) |>
+    lapply(.compute_ctc_one_cluster,
+           drift = .par[["drift"]],
+           covmax = .par[["covmax"]],
+           jgrow = .par[["jgrow"]],
+           SNK = .par[["SNK"]],
+           dact_m = .par[["dact_m"]],
+           css_ppm = .par[["css_ppm"]],
+           foc = .par[["foc"]],
+           bd_g_cm3 = .par[["bd_g_cm3"]],
+           qseep_m_day = .par[["qseep_m_day"]],
+           wilting = .par[["wilting"]],
+           fc = .par[["fc"]]
+           ) |>
+      data.table::rbindlist() |>
+      as.data.frame()
+}
+
+.compute_ctc_one_cluster <- function(cluster_ca_df,
+                                     drift,
+                                     covmax,
+                                     jgrow,
+                                     SNK,
+                                     dact_m,
+                                     css_ppm,
+                                     foc,
+                                     bd_g_cm3,
+                                     qseep_m_day,
+                                     wilting,
+                                     fc
+                                     )
+{
+  res_template <- as.list(cluster_ca_df[, c("cluster_id", "date")])
 
   chemicals <- unique(erahumed::albufera_ca_schedules$chemical)
   chemicals <- names(cluster_ca_df)[names(cluster_ca_df) %in% chemicals]
 
-  lapply(chemicals, \(chemical)
-         c(res_template, list(chemical = chemical), compute_masses(chemical))
-         ) |>
+  lapply(chemicals, function(chemical) {
+      masses <- ct_time_series(
+        application_kg = cluster_ca_df[[chemical]],
+        precipitation_mm = cluster_ca_df[["precipitation_mm"]],
+        etp_mm = cluster_ca_df[["evapotranspiration_mm"]],
+        temperature_ave = cluster_ca_df[["temperature_ave"]],
+        temperature_min = cluster_ca_df[["temperature_min"]],
+        temperature_max = cluster_ca_df[["temperature_max"]],
+        height_eod_cm = cluster_ca_df[["height_eod_cm"]],
+        outflow_m3_s = cluster_ca_df[["outflow_m3_s"]],
+        inflows_m3_s = list(cluster_ca_df[["inflow_m3_s"]]),
+        inflows_densities_kg_m3 = list(0),
+        area_m2 = cluster_ca_df[["area_m2"]][[1]],
+        seed_day = cluster_ca_df[["seed_day"]],
+        chemical = chemical,
+        drift = drift,
+        covmax = covmax,
+        jgrow = jgrow,
+        SNK = SNK,
+        dact_m = dact_m,
+        css_ppm = css_ppm,
+        foc = foc,
+        bd_g_cm3 = bd_g_cm3,
+        qseep_m_day = qseep_m_day,
+        wilting = wilting,
+        fc = fc
+        )
+      c(res_template, list(chemical = chemical), masses)
+    }) |>
     data.table::rbindlist() |>
     as.data.frame()
 }
