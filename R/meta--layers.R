47932a7f736db1b5ceaa8e90586cd26fd97497f4
Valerio Gherardi <vgherard840@gmail.com>
2025-03-26 13:46:47 +0100

    Remove confusing comment

--- R/meta--layers.R
+++ R/meta--layers.R
@@ -1,17 +1,3 @@
-#' ERAHUMED simulation layers
-#'
-#' @description
-#' Enumerates the ERAHUMED simulation layers
-#' (*cf.* \link{erahumed_simulation_interface}), in dependency order (from
-#' upstream to downstream). A schematic view is available
-#' [on the package website](https://erahumed.github.io/erahumed/articles/pipeline-scheme.html).
-#'
-#' @return
-#' A character vector.
-#'
-#' @details For further information on the various layers, see `?<layer_name>`.
-#'
-#' @noRd
 erahumed_layers <- function() {
   names(erahumed_input_docs("layers"))
 }
