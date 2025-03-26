7cdf89447c58861c942542c43fae5e507a47d5fa
Valerio Gherardi <vgherard840@gmail.com>
2025-03-26 13:24:13 +0100

    Modify chemicals palette

--- R/aux--plot.R
+++ R/aux--plot.R
@@ -1,7 +1,6 @@
 chemical_color_map <- function() {
   chemicals <- names(info_chemicals())
-  res <- grDevices::palette.colors(length(chemicals))
+  res <- grDevices::palette.colors(length(chemicals), palette = "Classic Tableau")
   names(res) <- chemicals
   return(res)
 }
-
