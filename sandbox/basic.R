6c4ee5e97440b441d3ceb4f7047432214e4e4732
Valerio Gherardi <vgherard840@gmail.com>
2025-03-28 08:39:37 +0100

    Updatr sandbox/basic.R

--- sandbox/basic.R
+++ sandbox/basic.R
@@ -1,11 +1,3 @@
 library(erahumed)
 
-date_start <- "2020-01-01"
-date_end <- "2020-01-10"
-outflows_df <- albufera_outflows |>
-  (\(.) .[date_start <= .$date & .$date <= date_end, ])()
-
-sim <- erahumed_simulation() |>
-  setup_hydrology(outflows_df = outflows_df)
-
-sim <- run_simulation(sim)
+sim <- erahumed_simulation(date_start = "2020-01-01", date_end = "2020-01-10")
