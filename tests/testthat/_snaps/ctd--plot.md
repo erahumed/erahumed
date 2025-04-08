de11bd2c9e5795b68178e47ddb9d2a2be73f8c3c
Valerio Gherardi <vgherard840@gmail.com>
2025-04-08 11:18:52 +0200

    Test ctd plot function

---
+++ tests/testthat/_snaps/ctd--plot.md
@@ -0,0 +1,5 @@
+# Snapshot is constant
+
+    Code
+      plot_ctd(test_sim_small(), element_id = info_ditches()$element_id[1])
+
---
+++ tests/testthat/test-ctd--plot.R
@@ -0,0 +1,23 @@
+test_that("No error with basic valid inputs", {
+  plot_ctd(test_sim_small(), element_id = info_ditches()$element_id[1]) |>
+    expect_no_error()
+})
+
+test_that("No error for all plot types and variables", {
+  common_args <- list(simulation = test_sim_small(),
+                      element_id = info_ditches()$element_id[1])
+
+  expect_no_error( do.call(plot_ctd, c(common_args, compartment = "water")) )
+  expect_no_error( do.call(plot_ctd, c(common_args, compartment = "sediment")) )
+})
+
+test_that("Succeeds but warns if no cluster is specified", {
+  plot_ctd(test_sim_small()) |>
+    expect_no_error() |>
+    expect_warning()
+})
+
+test_that("Snapshot is constant", {
+  plot_ctd(test_sim_small(), element_id = info_ditches()$element_id[1]) |>
+    expect_snapshot()
+})
