a616c024d9d222b212ad2b1e3435734bd0bc73bd
Valerio Gherardi <vgherard840@gmail.com>
2025-04-10 11:41:11 +0200

    Defer definition of DSS input defaults to separate helper

--- R/dss--input-parameters.R
+++ R/dss--input-parameters.R
@@ -2,7 +2,7 @@ dss_input_seed <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("Seed for simulation", dss_input_tooltip("seed")),
-    value = eval(formals(erahumed_simulation)$seed),
+    value = dss_input_defaults()[["seed"]],
     step = 1
     )
 }
@@ -16,10 +16,7 @@ dss_input_date_range <- function(id, date_min = NULL, date_max = NULL) {
     inputId = id,
     label = shiny::p("Date Range", tltp),
     range = TRUE,
-    value = c(
-      eval(formals(erahumed_simulation)$date_start),
-      eval(formals(erahumed_simulation)$date_end)
-      ),
+    value = dss_input_defaults()[["date_range"]],
     minDate = date_min,
     maxDate = date_max
     )
@@ -29,7 +26,7 @@ dss_input_sc_intercept <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("Intercept [10\u{2076}\u{00B7}m\u{00B3}]", dss_input_tooltip("storage_curve_intercept_m3")),
-    value = eval(formals(erahumed_simulation)$storage_curve_intercept_m3) / 1e6,
+    value = dss_input_defaults()[["sc_intercept"]],
     min = 0,
     max = 50,
     step = 0.001
@@ -40,7 +37,7 @@ dss_input_sc_slope <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("Slope [10\u{2076}\u{00B7}m\u{00B2}]", dss_input_tooltip("storage_curve_slope_m2")),
-    value = eval(formals(erahumed_simulation)$storage_curve_slope_m2) / 1e6,
+    value = dss_input_defaults()[["sc_slope"]],
     min = 0,
     max = 100,
     step = 0.001
@@ -51,7 +48,7 @@ dss_input_petp_surface <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("PETP-surface [10\u{2076}\u{00B7}m\u{00B2}]", dss_input_tooltip("petp_surface_m2")),
-    value = eval(formals(erahumed_simulation)$petp_surface_m2) / 1e6,
+    value = dss_input_defaults()[["petp_surface"]],
     min = 0,
     max = 1000,
     step = 0.001
@@ -62,7 +59,7 @@ dss_input_ideal_flow_rate_cm <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("Ideal Flow Rate [cm]", dss_input_tooltip("ideal_flow_rate_cm")),
-    value = eval(formals(erahumed_simulation)$ideal_flow_rate_cm),
+    value = dss_input_defaults()[["ideal_flow_rate_cm"]],
     min = 0,
     max = 20,
     step = 0.5
@@ -73,7 +70,7 @@ dss_input_height_thresh_cm <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("Height Threshold [cm]", dss_input_tooltip("height_thresh_cm")),
-    value = eval(formals(erahumed_simulation)$height_thresh_cm),
+    value = dss_input_defaults()[["height_thresh_cm"]],
     min = 0,
     max = 10,
     step = 0.1
@@ -84,7 +81,7 @@ dss_input_ditch_level_m <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("Ditch Level [m]", dss_input_tooltip("ditch_level_m")),
-    value = eval(formals(erahumed_simulation)$ditch_level_m),
+    value = dss_input_defaults()[["ditch_level_m"]],
     min = 0,
     max = 2,
     step = 0.01
@@ -95,7 +92,7 @@ dss_input_drift <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("drift", dss_input_tooltip("drift")),
-    value = eval(formals(erahumed_simulation)$drift),
+    value = dss_input_defaults()[["drift"]],
     step = 0.01,
     min = 0,
     max = 1
@@ -106,7 +103,7 @@ dss_input_covmax <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("covmax", dss_input_tooltip("covmax")),
-    value = eval(formals(erahumed_simulation)$covmax),
+    value = dss_input_defaults()[["covmax"]],
     step = 0.01,
     min = 0,
     max = 1
@@ -117,7 +114,7 @@ dss_input_jgrow <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("jgrow", dss_input_tooltip("jgrow")),
-    value = eval(formals(erahumed_simulation)$jgrow),
+    value = dss_input_defaults()[["jgrow"]],
     step = 1,
     min = 0,
     max = 400
@@ -128,7 +125,7 @@ dss_input_dact_m <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("dact_m", dss_input_tooltip("dact_m")),
-    value = eval(formals(erahumed_simulation)$dact_m),
+    value = dss_input_defaults()[["dact_m"]],
     step = 0.01,
     min = 0,
     max = 1
@@ -139,7 +136,7 @@ dss_input_css_ppm <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("css_ppm", dss_input_tooltip("css_ppm")),
-    value = eval(formals(erahumed_simulation)$css_ppm),
+    value = dss_input_defaults()[["css_ppm"]],
     step = 1,
     min = 0,
     max = 500
@@ -150,7 +147,7 @@ dss_input_foc <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("foc", dss_input_tooltip("foc")),
-    value = eval(formals(erahumed_simulation)$foc),
+    value = dss_input_defaults()[["foc"]],
     step = 0.001,
     min = 0,
     max = 1
@@ -161,7 +158,7 @@ dss_input_bd_g_cm3 <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("bd_g_cm3", dss_input_tooltip("bd_g_cm3")),
-    value = eval(formals(erahumed_simulation)$bd_g_cm3),
+    value = dss_input_defaults()[["bd_g_cm3"]],
     step = 0.01,
     min = 0,
     max = 10
@@ -172,7 +169,7 @@ dss_input_qseep_m_day <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("qseep_m_day", dss_input_tooltip("qseep_m_day")),
-    value = eval(formals(erahumed_simulation)$qseep_m_day),
+    value = dss_input_defaults()[["qseep_m_day"]],
     step = 0.01,
     min = 0,
     max = 10
@@ -183,7 +180,7 @@ dss_input_porosity <- function(id) {
   shiny::numericInput(
     inputId = id,
     label = shiny::p("porosity", dss_input_tooltip("porosity")),
-    value = eval(formals(erahumed_simulation)$porosity),
+    value = dss_input_defaults()[["porosity"]],
     step = 0.01,
     min = 0,
     max = 1
@@ -191,8 +188,7 @@ dss_input_porosity <- function(id) {
 }
 
 dss_input_prop_variety_slider <- function(id) {
-  vprop_default <- eval(formals(erahumed::erahumed_simulation)$variety_prop)
-  value <- c(vprop_default[1], vprop_default[1] + vprop_default[2])
+
   tltp_txt <- paste0(
     "The two numbers on the slider represent the fraction of the crop surface ",
     "allocated to J. Sendra and the combined area of J. Sendra and Bomba rice ",
@@ -206,7 +202,7 @@ dss_input_prop_variety_slider <- function(id) {
     label = shiny::p("Variety proportion (J.Sendra : Bomba : Clearfield)", tltp),
     min = 0,
     max = 1,
-    value = value,
+    value = dss_input_defaults()[["prop_variety_12"]],
     step = 0.01
     )
 }
