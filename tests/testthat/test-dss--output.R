fdee51e23c0ed7775b25e0370320523af934602f
Valerio Gherardi <vgherard840@gmail.com>
2025-04-11 10:28:45 +0200

    dss--output: server tests

---
+++ tests/testthat/test-dss--output.R
@@ -0,0 +1,31 @@
+library(shiny, quietly = TRUE)
+
+test_that("UI succeeds", {
+  expect_no_error(dss_output_ui("ui"))
+})
+
+test_that("Server runs without error", {
+  expect_no_error( shiny::testServer(dss_output_server, {}) )
+})
+
+test_that("Plots are generated without any error", {
+  shiny::testServer(dss_output_server, {
+    session$setInputs(water_body = "lake")
+    expect_no_error(output$hb_plot_storage)
+    expect_no_error(output$hb_plot_flows)
+    expect_no_error(output$ct_plot_water)
+    expect_no_error(output$ct_plot_sediment)
+    expect_no_error(output$r_plot)
+  },
+  args = list(simulation = shiny::reactive(test_sim_small()))
+  )
+})
+
+test_that("Cluster informative text is generated correctly", {
+  shiny::testServer(dss_output_server, {
+    session$setInputs(water_body = info_clusters()$element_id[[1]])
+    expect_no_error(output$selected_wb_info)
+  },
+  args = list(simulation = shiny::reactive(test_sim_small()))
+  )
+})
