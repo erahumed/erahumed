57ac710f6335e96494927cb5f59c1b51f1d6910b
Valerio Gherardi <vgherard840@gmail.com>
2025-04-11 12:27:26 +0200

    Toy test for SSD computation

---
+++ tests/testthat/test-r--low-level.R
@@ -0,0 +1,20 @@
+test_that("risk_from_sdss produces the expected output in toy case", {
+  acetamiprid_at_0.5_acute_risk <-
+    exp(info_chemicals()$Acetamiprid$ssd_acute_mu) / 1e6
+
+  ct_output <- data.frame(
+    date = "2000-01-01",
+    element_id = "lake",
+    chemical = "Acetamiprid",
+    cw_kg_m3 = c(acetamiprid_at_0.5_acute_risk)
+  )
+
+  res <- risk_from_ssds(ct_output)
+
+  expect_equal(nrow(res), 2)
+
+  res_chem <- res |>
+    (\(.) .[.$stressor_type == "chemical", ])()
+
+  expect_equal(res_chem$paf_acute, 0.5)
+})
