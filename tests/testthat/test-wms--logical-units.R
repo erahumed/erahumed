f23a317bb690843fd602a71f0bbc427e82354c51
Valerio Gherardi <vgherard840@gmail.com>
2025-04-15 11:50:12 +0200

    Test wms_irrigation()

---
+++ tests/testthat/test-wms--logical-units.R
@@ -0,0 +1,17 @@
+test_that("wms_irrigation() succeeds", {
+  expect_no_error( wms_irrigation(numeric(10)) )
+})
+
+test_that("wms_irrigation() returns a logical vector of the correct size", {
+  n <- 840
+  res <- wms_irrigation(numeric(n))
+  expect_vector(res, ptype = logical(), size = n)
+})
+
+test_that("wms_irrigation() returns the expected output in simple case", {
+  x <-
+    c(4, 4, 2, 0, 0, 0, 1, 2, 1, 0, 0, 1, 2, 2, 1, 0, 0, 0, 2, 4, 4)
+  expected <-
+    c(T, T, F, F, F, F, T, T, F, F, F, T, T, T, F, F, F, F, T, T, T)
+  expect_equal(wms_irrigation(x), expected)
+})
