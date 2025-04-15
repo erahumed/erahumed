74d9d3159dc79f8faf56359f970ed1abd36e3db5
Valerio Gherardi <vgherard840@gmail.com>
2025-04-15 12:41:16 +0200

    Testing of wms_height_cm()

--- tests/testthat/test-wms--logical-units.R
+++ tests/testthat/test-wms--logical-units.R
@@ -33,3 +33,33 @@ test_that("wms_draining() returns the expected output in simple case", {
     c(T, T, T, T, F, F, F, F, T, T, F, F, F, T, T, T, F, F, F, F, T)
   expect_equal(wms_draining(x), expected)
 })
+
+test_that("wms_height_cm() succeeds with valid arguments", {
+  args <- formals(water_management_scheme) |> lapply(eval)
+  args <- args[ names(args)[names(args) %in% names(formals(wms_height_cm))] ]
+  args <- c(args, tancat = TRUE)
+
+  expect_no_error(do.call(wms_height_cm, args))
+})
+
+test_that("wms_height_cm() value is zero in emptyings", {
+  args <- formals(water_management_scheme) |> lapply(eval)
+  args <- args[ names(args)[names(args) %in% names(formals(wms_height_cm))] ]
+  args <- c(args, tancat = TRUE)
+
+  res <- do.call(wms_height_cm, args)
+  emptyings <- args$emptyings_yday
+
+  expect_true(all(res[emptyings] == 0))
+})
+
+test_that("wms_height_cm() maximum value is that expected", {
+  args <- formals(water_management_scheme) |> lapply(eval)
+  args <- args[ names(args)[names(args) %in% names(formals(wms_height_cm))] ]
+  args <- c(args, tancat = TRUE)
+
+  res <- do.call(wms_height_cm, args)
+  expected_max <- max(c(args$flow_height_cm, args$perellona_height_cm))
+
+  expect_equal(max(res), expected_max)
+})
