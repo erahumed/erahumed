f91be601e882da46dc6b68cf4bf9b5f687eec7a2
Valerio Gherardi <vgherard840@gmail.com>
2025-04-15 11:58:47 +0200

    Test wms_draining()

--- tests/testthat/test-wms--logical-units.R
+++ tests/testthat/test-wms--logical-units.R
@@ -15,3 +15,21 @@ test_that("wms_irrigation() returns the expected output in simple case", {
     c(T, T, F, F, F, F, T, T, F, F, F, T, T, T, F, F, F, F, T, T, T)
   expect_equal(wms_irrigation(x), expected)
 })
+
+test_that("wms_draining() succeeds", {
+  expect_no_error( wms_draining(numeric(10)) )
+})
+
+test_that("wms_draining() returns a logical vector of the correct size", {
+  n <- 840
+  res <- wms_draining(numeric(n))
+  expect_vector(res, ptype = logical(), size = n)
+})
+
+test_that("wms_draining() returns the expected output in simple case", {
+  x <-
+    c(4, 4, 2, 0, 0, 0, 1, 2, 1, 0, 0, 1, 2, 2, 1, 0, 0, 0, 2, 4, 4)
+  expected <-
+    c(T, T, T, T, F, F, F, F, T, T, F, F, F, T, T, T, F, F, F, F, T)
+  expect_equal(wms_draining(x), expected)
+})
