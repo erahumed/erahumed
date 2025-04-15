8238692ab803d4758c37f20860b6361e161ca9d5
Valerio Gherardi <vgherard840@gmail.com>
2025-04-15 13:31:42 +0200

    Stub of "rv" concepts

--- R/rv--s3-class.R
+++ R/rv--s3-class.R
@@ -1,47 +1,94 @@
-chemical_application_scheme <- function(...) {
-  # Check that every
+rice_variety <- function(water_management_scheme = water_management_scheme(),
+                         # TODO el ... no sirve pa na si no exporto tmb un constructor de "chemical_application()",
+                         # pero la idea era solo exportar el "add_application" sin hacer explicita la clase...
+                         ...)
+{
+  tryCatch(
+    {
+      assert_water_management_scheme(water_management_scheme)
+      for (x in list(...)) {
+        assert_chemical_application(x)
+      }
+    },
+    error = function(e) {
+      class(e) <- c("erahumed_rv_error", class(e))
+      stop(e)
+    })
 
-  res <- list(...)
+  res <- list(water_management_scheme = water_management_scheme,
+              applications = list()
+              )
+  class(res) <- "erahumed_rice_variety"
 
-  class(res) <- "erahumed_ca_scheme"
+  for (application in list(...)) {
+    res <- add_application0(res, application)
+  }
+
+
 
   return(res)
 }
 
 add_application <- function(
-    chemical_application_scheme,
+    rice_variety,
     chemical,
     amount_kg_ha,
-    day,
-    type = c("ground", "aerial"),
-    variety = c("J.Sendra", "Bomba", "Clearfield")
+    seed_day,
+    type = c("ground", "aerial")
     )
+{
+  assert_rice_variety(rice_variety)
+
+  application <- chemical_application(chemical = chemical,
+                                      amount_kg_ha = amount_kg_ha,
+                                      seed_day = seed_day,
+                                      type = type
+                                      )
+
+  add_application0(rice_variety, application)
+
+  return(rice_variety)
+}
+
+add_application0 <- function(rice_variety, application) {
+  assert_rice_variety(rice_variety)
+  assert_chemical_application(application)
+  check_compatible_application(rice_variety, application)
+  rice_variety$applications <- c(rice_variety$applications, application)
+  return(rice_variety)
+}
+
+chemical_application <- function(chemical,
+                                 amount_kg_ha,
+                                 seed_day,
+                                 type = c("ground", "aerial")
+                                 )
 {
   tryCatch(
     {
-      # TODO: assert chemical_application_scheme
-      # TODO: assert chemical
+      assert_chemical(chemical)
 
       assert_positive_number(amount_kg_ha)
 
-      assert_numeric_vector(day)
-      assert_length_one(day)
+      assert_numeric_vector(seed_day)
+      assert_length_one(seed_day)
 
       if (missing(type))
         stop("Please specify application type")
       type <- match.arg(type)
 
-      if (missing(variety))
-        stop("Please specify application variety")
-      variety <- match.arg(variety)
-
       # TODO: check availability of day for specified application type and variety
       # Requires an argument with the management plan?
       # Or maybe the chemical application scheme could be joined together with the rice variety?
-      },
+    },
     error = function(e) {
       class(e) <- c("erahumed_chemical_application_error", class(e))
       stop(e)
     })
 
+  res <- list(chemical = chemical,
+              amount_kg_ha = amount_kg_ha,
+              seed_day = seed_day,
+              type = type)
+  class(res) <- "erahumed_chemical_application"
 }
