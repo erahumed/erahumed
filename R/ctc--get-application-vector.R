65ab7dbcee9a632d02f6626bf3b4e95e6e968935
Valerio Gherardi <vgherard840@gmail.com>
2025-05-27 12:55:36 +0200

    Fix bug

--- R/ctc--get-application-vector.R
+++ R/ctc--get-application-vector.R
@@ -1,10 +1,11 @@
 get_application_kg_vector <- function(
     chemical, applications_df, seed_day, plan_delay, area_m2
-    )
+)
 {
   res <- numeric(length(seed_day))
-  for (k in seq_along(applications_df)) {
-    if (applications_df$chemical[[k]] != chemical)
+
+  for (k in seq_len(nrow(applications_df))) {
+    if (applications_df$chemical_id[[k]] != chemical)
       next
     application_seed_day <- applications_df$seed_day[[k]]
     idx <- which.max(seed_day - plan_delay == application_seed_day)
