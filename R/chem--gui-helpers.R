7c2340775868fba5fce309549ff870b956f2f411
Valerio Gherardi <vgherard840@gmail.com>
2025-06-05 12:18:38 +0200

    Chemical GUI

---
+++ R/chem--gui-helpers.R
@@ -0,0 +1,12 @@
+default_chemical_db <- function() {
+  list(
+    acetamiprid(),
+    azoxystrobin(),
+    bentazone(),
+    cycloxydim(),
+    cyhalofop_butyl(),
+    difenoconazole(),
+    mcpa(),
+    penoxsulam()
+  )
+}
