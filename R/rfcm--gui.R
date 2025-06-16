f2b41d427aa35f86d0d3e10e4f18b175e726a004
Valerio Gherardi <vgherard840@gmail.com>
2025-06-16 12:06:28 +0200

    RFCM GUI with tabs

--- R/rfcm--gui.R
+++ R/rfcm--gui.R
@@ -3,9 +3,19 @@ rfcm_ui <- function(id) {
   shiny::tagList(
     chemical_db_ui(ns("chem_db")),
     shiny::hr(),
-    shiny::actionButton(ns("addButton"), "", icon = shiny::icon("plus"))
+    shiny::actionButton(ns("addButton"), "Add cluster", icon = shiny::icon("plus")),
+    shiny::tabsetPanel(
+      id = ns("rfms_tabs"),
+      type = "tabs"
+      # Initially empty; tabs will be inserted dynamically
+    )
   )
 }
+
+
+
+
+
 
 rfcm_server <- function(id, chemical_db, initial_rfms_list = list()) {
   shiny::moduleServer(id, function(input, output, session) {
@@ -13,16 +23,30 @@ rfcm_server <- function(id, chemical_db, initial_rfms_list = list()) {
 
     chemical_db <- chemical_db_server("chem_db")
 
-    shiny::observeEvent(input$addButton, {
-      id <- paste0("rfms-", input$addButton)
-      shiny::insertUI(
-        selector = paste0("#", ns("addButton")),
-        where = "beforeBegin",
-        ui = rfms_ui(ns(id)),
-        session = session
-      )
-      rfms_server(id = id, chemical_db = chemical_db, initial_rfms = jsendra())
+    counter <- shiny::reactiveVal(0)
 
+    shiny::observeEvent(input$addButton, {
+      i <- counter() + 1
+      counter(i)
+
+      tab_id <- paste0("rfms_", i)
+      tab_title <- paste("Cluster", i)
+
+      # Create the tab panel UI with namespaced ID
+      shiny::insertTab(
+        inputId = "rfms_tabs",
+        shiny::tabPanel(
+          title = tab_title,
+          value = tab_id,
+          rfms_ui(ns(tab_id))
+        ),
+        target = NULL,  # append at end
+        position = "after",
+        select = TRUE  # automatically switch to new tab
+      )
+
+      # Call the server logic for this rfms instance
+      rfms_server(id = tab_id, chemical_db = chemical_db, initial_rfms = jsendra())
     })
   })
 }
