60a815cf04df00ff77c6e39d982dd1bb905b13de
Valerio Gherardi <vgherard840@gmail.com>
2025-08-11 10:08:21 +0200

    Add references to technical documentation of algorithms (close #46)

--- R/rfms--docs.R
+++ R/rfms--docs.R
@@ -1,11 +1,12 @@
 #' Rice Field Management System
 #'
-#' Functions to define and initialize rice field management systems.
+#' Functions to define and initialize rice field management systems (RFMSs).
 #'
 #' @return An object of class `erahumed_rfms`.
 #'
-#' @details These functions are used to define and initialize rice field
-#' management system. Specifically:
+#' @details
+#' These functions are used to define and initialize rice field
+#' management systems. Specifically:
 #'
 #' * `new_rfms()` creates an empty management system, with no
 #'   scheduled chemical applications.
@@ -17,6 +18,10 @@
 #' [schedule_application()]. The default values of the arguments of
 #' `new_rfms()` coincide with those used internally by `jsendra()`,
 #' `bomba()`, and `clearfield()`.
+#'
+#' For a detailed explanation of RFMS concepts, configuration, and
+#' built-in presets, see the
+#' [RFMS section of the user manual](https://erahumed.github.io/erahumed-book/chapters/rfms.html).
 #'
 #' @seealso [schedule_application]
 #'
