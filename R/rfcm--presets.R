96ad6f761edb8fcaf2f5d08d38f59afdf4a1f279
Valerio Gherardi <vgherard840@gmail.com>
2025-10-13 11:35:12 +0200

    Change default RFMS map (close #494)

--- R/rfcm--presets.R
+++ R/rfcm--presets.R
@@ -2,12 +2,9 @@
 #' @name rfms_map
 #' @export
 default_rfms_map <- function(seed = 840) {
-
   withr::with_seed(seed, {
     new_rfms_map(default_rfms = jsendra()) |>
-      allocate_surface(clearfield(), target_fraction = 0.8, ditches = 1:9) |>
-      allocate_surface(clearfield(), target_fraction = 0.2, ditches = 10:26)
-    })
-
-
+      allocate_surface(bomba(), target_fraction = 0.2, field_type = "tancat") |>
+      allocate_surface(clearfield(), target_fraction = 0.4, ditches = 1:19)
+  })
 }
